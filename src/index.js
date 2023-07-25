5e2da291b5fc83a2c392ce7e39c292664bfc4bc4
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-25 19:51:41 +0200

    reformatted document

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 async function scrapData() {
-    const browser = await puppeteer.launch({ headless: 'new'});
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
 
-    await page.goto('https://tradingview.com/symbols/GPW-CDR/');
-    await page.screenshot({ path: 'test.png', type: "png" });
+  await page.goto("https://tradingview.com/symbols/GPW-CDR/");
+  await page.screenshot({ path: "test.png", type: "png" });
 
-    await browser.close();
+  await browser.close();
 }
 
 scrapData();
