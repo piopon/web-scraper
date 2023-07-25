7cad0d16f4a50446ff1b0265290166ea1b3c8001
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-25 19:45:25 +0200

    added initial method to take screenshot of hardcoded sample webpage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello from web-scraper!")
+const puppeteer = require('puppeteer');
+
+async function scrapData() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://tradingview.com/symbols/GPW-CDR/');
+    await page.screenshot({ path: 'test.png', type: "png" });
+
+    await browser.close();
+}
+
+scrapData();
