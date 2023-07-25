907fbdd1c5135b5aa72dc857da8e05405ce7e50c
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-26 01:40:14 +0200

    updated logic to use multiple pages array to scrap data from

--- src/scraper.js
+++ src/scraper.js
@@ -5,25 +5,39 @@ import fs from 'fs';
 
 let intervalId = undefined;
 
+const sourcePages = [
+  "https://www.tradingview.com/symbols/GPW-CDR/",
+  "https://www.tradingview.com/symbols/NYSE-T/"
+]
+
 async function scrapData() {
+  const data = [];
   const browser = await puppeteer.launch({ headless: "new" });
   const page = await browser.newPage();
 
-  await page.goto("https://www.tradingview.com/symbols/NYSE-T/");
+  for (let i = 0; i < sourcePages.length; i++) {
+    await page.goto(sourcePages[i]);
+    await page.waitForSelector('span[class^="last"] span', {visible: true});
+    const obj = await page.evaluate(() => {
+      const dataContainer = document.querySelector('div[class^="symbolRow"]');
+      return {
+          name: dataContainer.querySelector('h1').innerHTML,
+          price: dataContainer.querySelector('span[class^="last"] span').innerHTML
+      };
+    });
+    data.push(obj);
+  }
 
-  const data = await page.evaluate(() => {
-    const dataContainer = document.querySelector('div[class^="symbolRow"]');
-    return {
-        name: dataContainer.querySelector('h1').innerHTML,
-        price: dataContainer.querySelector('span[class^="last"] span').innerHTML
-    };
-  });
+  await page.close();
+  await browser.close();
+
   const fileContent = [
     {
-      group : "tradingview.com",
-      items : [ data ],
+      domain : "tradingview.com",
+      items : data,
     }
   ];
+
   const dataDirectory = path.join(path.dirname(url.fileURLToPath(import.meta.url)), '..', 'data');
   if (!fs.existsSync(dataDirectory)) {
     fs.mkdirSync(dataDirectory);
@@ -31,8 +45,6 @@ async function scrapData() {
   fs.writeFile('../data/data.json', JSON.stringify(fileContent, null, 2), (err) => {
     if (err) throw err;
   })
-
-  await browser.close();
 }
 
 export function start() {
