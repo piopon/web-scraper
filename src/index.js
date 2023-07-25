b935dbf2db83d7279a882d0d9f95ff878181d059
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-25 22:03:06 +0200

    calling scraping logic from scraper.js file

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-import puppeteer from "puppeteer";
+import * as scraper from "./scraper.js";
 
-let intervalId = undefined;
-
-async function scrapData() {
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-
-  await page.goto("https://tradingview.com/symbols/GPW-CDR/");
-
-  const data = await page.evaluate(() => {
-    const dataContainer = document.querySelector('div[class^="symbolRow"]');
-    return {
-        name: dataContainer.querySelector('h1').innerHTML,
-        price: dataContainer.querySelector('span[class^="last"] span').innerHTML
-    };
-  });
-  console.log(data);
-
-  await browser.close();
-}
-
-function start() {
-    scrapData();
-    intervalId = setInterval(scrapData, 30_000);
-}
-
-function stop() {
-    if (intervalId !== undefined) {
-        clearInterval(intervalId);
-    }
-}
-
-start();
+scraper.start();
