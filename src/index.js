e6f390295fcec8235f18e8580f96edf7f14daebd
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-25 21:30:20 +0200

    added logic to scrap sample data

--- src/index.js
+++ src/index.js
@@ -5,7 +5,15 @@ async function scrapData() {
   const page = await browser.newPage();
 
   await page.goto("https://tradingview.com/symbols/GPW-CDR/");
-  await page.screenshot({ path: "test.png", type: "png" });
+
+  const data = await page.evaluate(() => {
+    const dataContainer = document.querySelector('div[class^="symbolRow"]');
+    return {
+        name: dataContainer.querySelector('h1').innerHTML,
+        price: dataContainer.querySelector('span[class^="last"] span').innerHTML
+    };
+  });
+  console.log(data);
 
   await browser.close();
 }
