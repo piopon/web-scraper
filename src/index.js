79fc568dc05a4f7ff5e2f74a4c984687f661a355
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-25 19:51:25 +0200

    fixed new headless mode when launching new browser

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 
 async function scrapData() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ headless: 'new'});
     const page = await browser.newPage();
 
     await page.goto('https://tradingview.com/symbols/GPW-CDR/');
