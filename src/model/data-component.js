60114e1fb1c30f10e1b2148a2852bd4fbd871aef
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-27 00:06:27 +0200

    added new DataComponent class with getters for each field

---
+++ src/model/data-component.js
@@ -0,0 +1,24 @@
+export class DataComponent {
+    #interval;
+    #selector;
+    #attribute;
+
+    constructor(object) {
+        object = object != null ? object : {};
+        this.#interval = object.interval != null ? object.interval : "";
+        this.#selector = object.selector != null ? object.selector : "";
+        this.#attribute = object.attribute != null ? object.attribute : "";
+    }
+
+    getInterval() {
+        return this.#interval;
+    }
+
+    getSelector() {
+        return this.#selector;
+    }
+
+    getAttribute() {
+        return this.#attribute;
+    }
+}
