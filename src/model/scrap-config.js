52aaba62b0397d3267c587ac51e4307b643c7875
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-27 21:06:14 +0200

    added correct defs for scrap group and config using new model utils functions

--- src/model/scrap-config.js
+++ src/model/scrap-config.js
@@ -1,3 +1,15 @@
+import { ScrapGroup } from "./scrap-group";
+import { getArrayOfModels } from '../components/model-utils'
+
 export class ScrapConfig {
     #groups;
+
+    constructor(object) {
+        object = object != null ? object : {};
+        this.#groups = getArrayOfModels(ScrapGroup, object);
+    }
+
+    getGroups() {
+        return this.#groups;
+    }
 }
--- src/model/scrap-group.js
+++ src/model/scrap-group.js
@@ -1,5 +1,27 @@
+import { ScrapObserver } from "./scrap-observer";
+import { getArrayOfModels } from '../components/model-utils'
+
 export class ScrapGroup {
     #name;
     #domain;
     #observers;
+
+    constructor(object) {
+        object = object != null ? object : {};
+        this.#name = object.name != null ? object.name : "";
+        this.#domain = object.domain != null ? object.domain : "";
+        this.#observers = getArrayOfModels(ScrapObserver, object.observers);
+    }
+
+    getName() {
+        return this.#name;
+    }
+
+    getDomain() {
+        return this.#domain;
+    }
+
+    getObservers() {
+        return this.#observers;
+    }
 }
