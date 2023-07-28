23b81f0e0e8cf5e668bf4cc4a5d79e92d66cc4b5
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-28 19:13:18 +0200

    updated scraper logic by putting it in a WebScraper class

--- src/components/scraper.js
+++ src/components/scraper.js
@@ -1,60 +1,62 @@
 import puppeteer from "puppeteer";
-import path from 'path';
-import url from 'url';
-import fs from 'fs';
+import path from "path";
+import url from "url";
+import fs from "fs";
 
-let intervalId = undefined;
+export class WebScraper {
+  #intervalId = undefined;
+  #scrapConfig = undefined;
 
-const sourcePages = [
-  "https://www.tradingview.com/symbols/GPW-CDR/",
-  "https://www.tradingview.com/symbols/NYSE-T/"
-]
+  constructor(object) {
+    this.#scrapConfig = new ScrapComponent(object);
+  }
 
-async function scrapData() {
-  const data = [];
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
+  async #scrapData() {
+    const data = [];
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
 
-  for (let i = 0; i < sourcePages.length; i++) {
-    await page.goto(sourcePages[i]);
-    await page.waitForSelector('span[class^=last] span', {visible: true});
-    const obj = await page.evaluate(() => {
-      const dataContainer = document.querySelector('div[class^=symbolRow]');
-      return {
-        name: dataContainer.querySelector('h1').innerHTML,
-        icon: dataContainer.querySelector('img[class^=tv-circle-logo]').src,
-        price: dataContainer.querySelector('span[class^=last] span').innerHTML
-      };
+    for (let i = 0; i < sourcePages.length; i++) {
+      await page.goto(sourcePages[i]);
+      await page.waitForSelector("span[class^=last] span", { visible: true });
+      const obj = await page.evaluate(() => {
+        const dataContainer = document.querySelector("div[class^=symbolRow]");
+        return {
+          name: dataContainer.querySelector("h1").innerHTML,
+          icon: dataContainer.querySelector("img[class^=tv-circle-logo]").src,
+          price: dataContainer.querySelector("span[class^=last] span").innerHTML,
+        };
+      });
+      data.push(obj);
+    }
+
+    await page.close();
+    await browser.close();
+
+    const fileContent = [
+      {
+        name: "stock",
+        items: data,
+      },
+    ];
+
+    const dataDirectory = path.join(path.dirname(url.fileURLToPath(import.meta.url)), "..", "data");
+    if (!fs.existsSync(dataDirectory)) {
+      fs.mkdirSync(dataDirectory);
+    }
+    fs.writeFile("../data/data.json", JSON.stringify(fileContent, null, 2), (err) => {
+      if (err) throw err;
     });
-    data.push(obj);
   }
 
-  await page.close();
-  await browser.close();
-
-  const fileContent = [
-    {
-      name : "stock",
-      items : data,
-    }
-  ];
-
-  const dataDirectory = path.join(path.dirname(url.fileURLToPath(import.meta.url)), '..', 'data');
-  if (!fs.existsSync(dataDirectory)) {
-    fs.mkdirSync(dataDirectory);
+  start() {
+    this.#scrapData();
+    this.#intervalId = setInterval(this.#scrapData, 30_000);
   }
-  fs.writeFile('../data/data.json', JSON.stringify(fileContent, null, 2), (err) => {
-    if (err) throw err;
-  })
-}
 
-export function start() {
-    scrapData();
-    intervalId = setInterval(scrapData, 30_000);
-}
-
-export function stop() {
-    if (intervalId !== undefined) {
-        clearInterval(intervalId);
+  stop() {
+    if (this.#intervalId !== undefined) {
+      clearInterval(intervalId);
     }
+  }
 }
