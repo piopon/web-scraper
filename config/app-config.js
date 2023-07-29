ac6e9d03596643c22ce2ef3aa9eecb6884efe4f8
Piotr Ponikowski <piopon.github@gmail.com>
2023-07-29 17:49:24 +0200

    added initial version of AppConfig class with scraping settings

---
+++ config/app-config.js
@@ -0,0 +1,21 @@
+import path from "path";
+import url from "url";
+
+export class AppConfig {
+  #currDir = undefined;
+  #rootDir = undefined;
+
+  constructor() {
+    this.#currDir = path.dirname(url.fileURLToPath(import.meta.url));
+    this.#rootDir = path.join(this.#currDir, "..");
+  }
+
+  getConfig() {
+    return {
+      scraperConfig: {
+        srcFile: path.join(this.#currDir, "scrap-config.json"),
+        dstFile: path.join(this.#rootDir, "data", "data.json"),
+      },
+    };
+  }
+}
