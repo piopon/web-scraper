f5523612f0ac9ac9be0fb79ebb1fc6ea0556f15f
Piotr Ponikowski <piopon.github@gmail.com>
2023-08-01 20:06:34 +0200

    added scrap observer required parameters (path & price) checks

--- src/model/scrap-observer.js
+++ src/model/scrap-observer.js
@@ -9,5 +9,22 @@ export class ScrapObserver {
         this.title = new ScrapComponent(input.title);
         this.image = new ScrapComponent(input.image);
         this.price = new ScrapComponent(input.price);
+        // check values only when object was passed (we use empty constructor call to get keys)
+        if (object) {
+            // path is needed so the scraper will know the exact page to scan
+            if (!this.path) {
+                throw Error(`Missing observer path`);
+            }
+            // all the price settings are essential (after all we want to get price information)
+            if (!this.price.selector) {
+                throw Error(`Missing 'price.selector' in observer ${object.path}`);
+            }
+            if (!this.price.attribute) {
+                throw Error(`Missing 'price.attribute' in observer ${object.path}`);
+            }
+            if(!this.price.auxiliary) {
+                throw Error(`Missing 'price.auxiliary' in observer ${object.path}`);
+            }
+        }
     }
 }
