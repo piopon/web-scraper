985d182b34686f333aaa3f0530a39877e9bd1fbf
Piotr Ponikowski <piopon.github@gmail.com>
2023-08-05 13:39:18 +0200

    corrected data router logic to return output all and filtered data content

--- src/routes/api/data.js
+++ src/routes/api/data.js
@@ -1,8 +1,13 @@
 import express from "express";
+import fs from "fs";
 
 export class DataRouter {
   #dataFilePath = undefined;
 
+  /**
+   * Creates a new data router for configuring appropriate endpoints
+   * @param {String} dataFile The path to the data file
+   */
   constructor(dataFile) {
     this.#dataFilePath = dataFile;
   }
@@ -15,11 +20,18 @@ export class DataRouter {
     const router = express.Router();
     // create endpoint for receiving all data
     router.get("/", (request, response) => {
-      response.status(200).json("getting all data");
+      var dataContent = JSON.parse(fs.readFileSync(this.#dataFilePath));
+      response.status(200).json(dataContent);
     });
     // create endpoint for receiving data with specified name
     router.get("/:name", (request, response) => {
-      response.status(200).json(`getting data with name: ${request.params.name}`);
+      var dataContent = JSON.parse(fs.readFileSync(this.#dataFilePath));
+      var searchedData = dataContent.filter((data) => data.name === request.params.name);
+      if (searchedData.length > 0) {
+        response.status(200).json(searchedData);
+      } else {
+        response.status(400).json(`No data with name: ${request.params.name}`);
+      }
     });
     return router;
   }
