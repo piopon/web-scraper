3cece9b90953138987de7050e041d66d479be2d6
Piotr Ponikowski <piopon.github@gmail.com>
2023-08-07 23:58:32 +0200

    added initial version of config router query params validation logic

--- src/routes/api/config-router.js
+++ src/routes/api/config-router.js
@@ -1,3 +1,4 @@
+import Ajv from "ajv";
 import express from "express";
 import fs from "fs";
 
@@ -19,6 +20,11 @@ export class ConfigRouter {
   createRoutes() {
     const router = express.Router();
     router.get("/", (request, response) => {
+      const validationResult = this.#validateQueryParams(request.url, request.query);
+      if (!validationResult.valid) {
+        response.status(400).json(validationResult.cause);
+        return;
+      }
       var configContent = JSON.parse(fs.readFileSync(this.#configFilePath));
       var filteredData = configContent.filter((data) => {
         return request.query.user ? data.user === parseInt(request.query.user) : true;
@@ -26,5 +32,21 @@ export class ConfigRouter {
       response.status(200).json(filteredData);
     });
     return router;
+  }
+
+  /**
+   * Method used to validate the config router endpoint query parameters
+   * @param {String} url The endpoint URL address
+   * @param {Object} params The query parameters which should be validated
+   * @returns an object with validation result (true/false) and an optional cause (if validation NOK)
+   */
+  #validateQueryParams(url, params) {
+    const validator = new Ajv();
+    const validate = validator.compile({
+      type: "object",
+      additionalProperties: false,
+      properties: { user: { type: "string" } },
+    });
+    return { valid: validate(params), cause: validate.errors };
   }
 }
