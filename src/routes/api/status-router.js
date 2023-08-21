c4c862d601056e2d22c770728b21e01b624229d7
Piotr Ponikowski <piopon.github@gmail.com>
2023-08-22 00:30:13 +0200

    added docs to newly created status router methods

--- src/routes/api/status-router.js
+++ src/routes/api/status-router.js
@@ -3,10 +3,18 @@ import express from "express";
 export class StatusRouter {
   #components = undefined;
 
+  /**
+   * Creates a new status router for configuring appropriate endpoints
+   * @param {Array} components The array of components which status we want to receive
+   */
   constructor(components) {
     this.#components = components;
   }
 
+  /**
+   * Method used to create routes for components status values
+   * @returns router object for handling status requests
+   */
   createRoutes() {
     const router = express.Router();
     router.get("/", (request, response) => {
