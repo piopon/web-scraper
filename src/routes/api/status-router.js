645ffedbefc8e15b98997127a00797dc46153f7d
Piotr Ponikowski <piopon.github@gmail.com>
2023-08-23 00:44:55 +0200

    adjusted get status endpoint to return web-server info + just alive state

--- src/routes/api/status-router.js
+++ src/routes/api/status-router.js
@@ -18,13 +18,12 @@ export class StatusRouter {
   createRoutes() {
     const router = express.Router();
     router.get("/", (request, response) => {
-      const outputData = [];
+      const outputData = [{ name: "web-server", alive: true }];
       for (let componentIndex = 0; componentIndex < this.#components.length; componentIndex++) {
         const component = this.#components[componentIndex];
         outputData.push({
           name: component.getName(),
           alive: component.isAlive(),
-          history: component.getStatusHistory(),
         });
       }
       response.status(200).json(outputData);
