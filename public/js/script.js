644b1416f7b8dea1831315dae6ad52e16782e404
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-08 00:18:24 +0200

    updated all variables names in main JS script file to increase readability

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,46 @@
 var ColumnsController = function () {
-  var tileLink = document.querySelectorAll(".group-column > .group-container");
-  var stripClose = document.querySelectorAll(".group-container > .group-column-close");
-  var expanded = false;
+  var groupColumns = document.querySelectorAll(".group-column > .group-container");
+  var groupCloseButtons = document.querySelectorAll(".group-container > .group-column-close");
+  var groupExpanded = false;
 
-  var open = function (elContent) {
-    if (!expanded) {
-      elContent.parentNode.classList.add("group-column-expanded");
-      elContent.querySelector(".group-content").style.transition = "all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)";
-      const closeBtn = elContent.querySelector(".group-column-close");
-      closeBtn.classList.add("group-column-close-show");
-      closeBtn.style.transition = "all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)";
-      expanded = true;
+  var expand = function (groupColumn) {
+    if (!groupExpanded) {
+      groupColumn.parentNode.classList.add("group-column-expanded");
+      groupColumn.querySelector(".group-content").style.transition = "all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)";
+      const groupCloseButton = groupColumn.querySelector(".group-column-close");
+      groupCloseButton.classList.add("group-column-close-show");
+      groupCloseButton.style.transition = "all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)";
+      groupExpanded = true;
     }
   };
 
-  var close = function (elContent) {
-    if (expanded) {
-      var elExpanded = elContent.parentNode.parentNode.parentNode.querySelector(".group-column-expanded");
-      elExpanded.querySelector(".group-content").style.transition = "all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)";
-      elExpanded.classList.remove("group-column-expanded");
-      elContent.classList.remove("group-column-close-show");
-      elContent.style.transition = "all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)";
-      expanded = false;
+  var collapse = function (groupCloseButton) {
+    if (groupExpanded) {
+      const groupColumn = groupCloseButton.parentNode.parentNode.parentNode.querySelector(".group-column-expanded");
+      groupColumn.querySelector(".group-content").style.transition = "all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)";
+      groupColumn.classList.remove("group-column-expanded");
+      groupCloseButton.classList.remove("group-column-close-show");
+      groupCloseButton.style.transition = "all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)";
+      groupExpanded = false;
     }
   };
 
-  var bindActions = function () {
-    [].forEach.call(tileLink, function (tlnk) {
-      tlnk.addEventListener("click", function (event) {
-        open(this);
+  var bindListeners = function () {
+    [].forEach.call(groupColumns, function (column) {
+      column.addEventListener("click", function (event) {
+        expand(this);
         event.stopPropagation();
       });
     });
-    [].forEach.call(stripClose, function (colClose) {
-      colClose.addEventListener("click", function (event) {
-        close(this);
+    [].forEach.call(groupCloseButtons, function (closeButton) {
+      closeButton.addEventListener("click", function (event) {
+        collapse(this);
         event.stopPropagation();
       });
     });
   };
 
-  return { init: bindActions };
+  return { initialize: bindListeners };
 };
 
-ColumnsController().init();
+ColumnsController().initialize();
