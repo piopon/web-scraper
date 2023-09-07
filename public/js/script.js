7f29ebff7b3284c577dd88c0778ad4828b0f5a5d
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-07 23:57:44 +0200

    corrected expanding and collapsing group columns

--- public/js/script.js
+++ public/js/script.js
@@ -7,21 +7,20 @@ var Expand = (function () {
     if (!expanded) {
       elContent.parentNode.classList.add("group-column-expanded");
       elContent.querySelector(".group-content").style.transition = "all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)";
-      stripClose.classList.add("group-column-close-show");
-      stripClose.style.transition = "all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)";
+      const closeBtn = elContent.querySelector(".group-column-close")
+      closeBtn.classList.add("group-column-close-show");
+      closeBtn.style.transition = "all .6s 1s cubic-bezier(0.23, 1, 0.32, 1)";
       expanded = true;
     }
   };
 
   var close = function (elContent) {
     if (expanded) {
-      var elExpanded = elContent.parentNode.querySelector(".group-column-expanded");
-      elContent.parentNode
-        .querySelector(".group-column-expanded")
-        .querySelector(".group-content").style.transition = "all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)";
+      var elExpanded = elContent.parentNode.parentNode.parentNode.querySelector(".group-column-expanded");
+      elExpanded.querySelector(".group-content").style.transition = "all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)";
       elExpanded.classList.remove("group-column-expanded");
-      stripClose.classList.remove("group-column-close-show");
-      stripClose.style.transition = "all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)";
+      elContent.classList.remove("group-column-close-show");
+      elContent.style.transition = "all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)";
       expanded = false;
     }
   };
