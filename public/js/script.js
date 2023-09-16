0d83e2509365ec847c4ff6f637b33c01518dc953
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-16 19:00:08 +0200

    added group collapsed class & updated JS to display opened group full view

--- public/js/script.js
+++ public/js/script.js
@@ -5,7 +5,7 @@ const ColumnsController = function () {
 
   const expand = function (groupColumn) {
     if (!groupExpanded) {
-      groupColumn.parentNode.classList.add("group-column-expanded");
+      groupColumns.forEach(column => column.parentNode.classList.add(column === groupColumn ? "group-column-expanded" : "group-column-collapsed"));
       groupColumn.querySelector(".group-content").style.transition = "all .5s .3s cubic-bezier(0.23, 1, 0.32, 1)";
       const groupCloseButton = groupColumn.querySelector(".group-column-close");
       groupCloseButton.classList.add("group-column-close-show");
@@ -20,6 +20,7 @@ const ColumnsController = function () {
         groupCloseButton.parentNode.parentNode.parentNode.parentNode.querySelector(".group-column-expanded");
       groupColumn.querySelector(".group-content").style.transition = "all 0.15s 0 cubic-bezier(0.23, 1, 0.32, 1)";
       groupColumn.classList.remove("group-column-expanded");
+      groupColumns.forEach(column => column.parentNode.classList.remove("group-column-collapsed"));
       groupCloseButton.classList.remove("group-column-close-show");
       groupCloseButton.style.transition = "all 0.2s 0s cubic-bezier(0.23, 1, 0.32, 1)";
       groupExpanded = false;
