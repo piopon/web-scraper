b792043514ad03aca3adab892cca69e2160fae04
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-17 10:22:49 +0200

    updated columns style controller to not have color duplicates

--- public/js/script.js
+++ public/js/script.js
@@ -33,8 +33,17 @@ const ColumnsController = function () {
     const colors = ["navy", "aqua", "green", "orange", "red", "blue", "yellow", "plum"];
     const animations = ["column-from-top", "column-from-right", "column-from-bottom", "column-from-left"];
     [].forEach.call(groupColumns, function (column) {
-      column.classList.add("background-" + colors[Math.floor(Math.random() * colors.length)]);
-      column.classList.add(animations[Math.floor(Math.random() * animations.length)]);
+      // get color from array and then remove it so no duplicates are selected (in next iterations)
+      const selectedColor = colors[Math.floor(Math.random() * colors.length)];
+      var index = colors.indexOf(selectedColor);
+      if (index !== -1) {
+        colors.splice(index, 1);
+      }
+      column.classList.add("background-" + selectedColor);
+      column.querySelector(".group-title").classList.add("background-" + selectedColor);
+      // get animation from array (duplicates are allowed in this case)
+      const selectedAnimation = animations[Math.floor(Math.random() * animations.length)];
+      column.classList.add(selectedAnimation);
     });
   };
 
