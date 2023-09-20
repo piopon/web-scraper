6462715c79de05d39ef1820ef2b8e4316085b547
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-20 20:50:20 +0200

    added column controller method descriptions

--- public/js/columns-controller.js
+++ public/js/columns-controller.js
@@ -3,6 +3,10 @@ export const ColumnsController = function () {
   const groupCloseButtons = document.querySelectorAll(".group-content > .group-column-close");
   var groupExpanded = false;
 
+  /**
+   * Method used to expand the selected group column to full available width
+   * @param {Object} groupColumn The column which should be expanded
+   */
   const expand = function (groupColumn) {
     if (!groupExpanded) {
       groupColumns.forEach((column) => {
@@ -14,6 +18,10 @@ export const ColumnsController = function () {
     }
   };
 
+  /**
+   * Method used to collapse the selected group column to display all available choices/columns
+   * @param {Object} groupCloseButton The close button of column which should be collapsed
+   */
   const collapse = function (groupCloseButton) {
     if (groupExpanded) {
       groupColumns.forEach((column) => {
@@ -26,6 +34,9 @@ export const ColumnsController = function () {
     }
   };
 
+  /**
+   * Method used to set style for all group columns (position, colors and animation)
+   */
   const setStyle = function () {
     const colors = ["navy", "aqua", "green", "orange", "red", "blue", "yellow", "plum"];
     const animations = ["column-from-top", "column-from-right", "column-from-bottom", "column-from-left"];
@@ -46,6 +57,10 @@ export const ColumnsController = function () {
     });
   };
 
+  /**
+   * Method used to set the position (and width) of the specified column
+   * @param {Object} column The column which position (and width) shoulde be setup
+   */
   const setPosition = function (column) {
     const columnWidth = 100 / groupColumns.length;
     const columnIndex = Array.from(groupColumns).indexOf(column);
@@ -53,10 +68,17 @@ export const ColumnsController = function () {
     column.parentNode.style.left = `${columnWidth * columnIndex}vh`;
   };
 
+  /**
+   * Method used to clear the position setting of the selected column
+   * @param {Object} column The column which position setting should be cleared
+   */
   const clearPosition = function (column) {
     column.parentNode.removeAttribute("style");
   };
 
+  /**
+   * Method used to bind listeners to all group columns and column close buttons
+   */
   const bindListeners = function () {
     [].forEach.call(groupColumns, function (column) {
       column.addEventListener("click", function (event) {
@@ -72,6 +94,9 @@ export const ColumnsController = function () {
     });
   };
 
+  /**
+   * Method used to setup the column controller (set style, position, create and bind all listeners)
+   */
   const setup = function () {
     setStyle();
     bindListeners();
