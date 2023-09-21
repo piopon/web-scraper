3c639ea0153a9102c93781593bedb4ca0b8b4278
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-21 19:39:46 +0200

    added array random element retrieval method & adjusted logic

--- public/js/columns-styler.js
+++ public/js/columns-styler.js
@@ -1,24 +1,36 @@
 export const ColumnsStyler = function () {
+  const colors = ["navy", "aqua", "green", "orange", "red", "blue", "yellow", "plum"];
+  const animations = ["column-from-top", "column-from-right", "column-from-bottom", "column-from-left"];
   const groupColumns = document.querySelectorAll(".group-column > .group-container");
+
+  /**
+   * Method used to receive random element from provided array
+   * @param {Object} array The array from which we want to receive element
+   * @param {Boolean} remove If we want to remove the received random element from array (to prevent duplicates)
+   * @returns random element from input array
+   */
+  const getRandom = function (array, remove) {
+    const randomItem = array[Math.floor(Math.random() * array.length)];
+    if (remove) {
+      var index = array.indexOf(randomItem);
+      if (index !== -1) {
+        array.splice(index, 1);
+      }
+    }
+    return randomItem;
+  };
 
   /**
    * Method used to set style for all group columns (position, colors and animation)
    */
   const initStyle = function () {
-    const colors = ["navy", "aqua", "green", "orange", "red", "blue", "yellow", "plum"];
-    const animations = ["column-from-top", "column-from-right", "column-from-bottom", "column-from-left"];
     groupColumns.forEach((column) => {
       // get color from array and then remove it so no duplicates are selected (in next iterations)
-      const selectedColor = colors[Math.floor(Math.random() * colors.length)];
-      var index = colors.indexOf(selectedColor);
-      if (index !== -1) {
-        colors.splice(index, 1);
-      }
+      const selectedColor = getRandom(colors, true);
       column.classList.add("background-" + selectedColor);
       column.querySelector(".group-title").classList.add("background-" + selectedColor);
       // get animation from array (duplicates are allowed in this case)
-      const selectedAnimation = animations[Math.floor(Math.random() * animations.length)];
-      column.classList.add(selectedAnimation);
+      column.classList.add(getRandom(animations, false));
     });
   };
 
