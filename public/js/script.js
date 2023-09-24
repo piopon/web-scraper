f8965390b94e0dd9b6ca16fb4da979547fa8cc50
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-24 15:24:28 +0200

    added JS logic to expand/collapse component card

--- public/js/script.js
+++ public/js/script.js
@@ -10,5 +10,27 @@ observersController.initialize();
 groupsController.initialize();
 groupsStyler.initialize();
 
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    if (!card.hasAttribute('active')) {
+      updateActiveCard(card);
+    }
+  });
+});
+
+function updateActiveCard(activeCard) {
+  cards.forEach((card) => {
+    if (card === activeCard) {
+      card.setAttribute('active', '');
+    } else {
+      card.removeAttribute('active');
+    }
+  })
+}
+
+
 window.addObserver = observersController.add;
 window.updateObserver = observersController.update;
