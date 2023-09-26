efedbb13f940ca6dfef36dc499ffd06d25d9cc97
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-26 23:08:33 +0200

    moved component cards logic to a new file & updated references

---
+++ public/js/components-controller.js
@@ -0,0 +1,25 @@
+export const ComponentsController = function () {
+  const cards = document.querySelectorAll(".card");
+
+  const bindListeners = function () {
+    cards.forEach((card) => {
+      card.addEventListener("click", () => {
+        if (!card.hasAttribute("active")) {
+          updateActiveCard(card);
+        }
+      });
+    });
+
+    function updateActiveCard(activeCard) {
+      cards.forEach((card) => {
+        if (card === activeCard) {
+          card.setAttribute("active", "");
+        } else {
+          card.removeAttribute("active");
+        }
+      });
+    }
+  };
+
+  return { initialize: bindListeners };
+};
--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,17 @@
+import { ComponentsController } from "./components-controller.js";
 import { ObserversController } from "./observers-controller.js";
 import { GroupsController } from "./groups-controller.js";
 import { GroupsStyler } from "./groups-styler.js";
 
+const componentsController = ComponentsController();
 const observersController = ObserversController();
 const groupsController = GroupsController();
 const groupsStyler = GroupsStyler();
 
+componentsController.initialize();
 observersController.initialize();
 groupsController.initialize();
 groupsStyler.initialize();
-
-
-const cards = document.querySelectorAll('.card');
-
-cards.forEach((card) => {
-  card.addEventListener('click', () => {
-    if (!card.hasAttribute('active')) {
-      updateActiveCard(card);
-    }
-  });
-});
-
-function updateActiveCard(activeCard) {
-  cards.forEach((card) => {
-    if (card === activeCard) {
-      card.setAttribute('active', '');
-    } else {
-      card.removeAttribute('active');
-    }
-  })
-}
-
 
 window.addObserver = observersController.add;
 window.updateObserver = observersController.update;
