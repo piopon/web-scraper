81e7e5ec8abb3c94e9627921472c05993400f028
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-29 23:11:13 +0200

    removed groups-styler content + removed not needed references

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,14 @@
 import { ComponentsController } from "./components-controller.js";
 import { ObserversController } from "./observers-controller.js";
 import { GroupsController } from "./groups-controller.js";
-import { GroupsStyler } from "./groups-styler.js";
 
 const componentsController = ComponentsController();
 const observersController = ObserversController();
 const groupsController = GroupsController();
-const groupsStyler = GroupsStyler();
 
 componentsController.initialize();
 observersController.initialize();
 groupsController.initialize();
-groupsStyler.initialize();
 
 window.addObserver = observersController.add;
 window.updateObserver = observersController.update;
