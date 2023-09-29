ec3a1157fddec27b3983a93dfb3b8eb2fab8359c
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-29 23:10:42 +0200

    moved group style logic to controller file

--- public/js/groups-controller.js
+++ public/js/groups-controller.js
@@ -1,7 +1,9 @@
 export const GroupsController = function () {
+  var groupExpanded = false;
+  const colors = ["navy", "aqua", "green", "orange", "red", "blue", "yellow", "plum"];
+  const animations = ["column-from-top", "column-from-right", "column-from-bottom", "column-from-left"];
   const groupColumns = document.querySelectorAll(".group-column > .group-container");
   const groupCloseButtons = document.querySelectorAll(".group-content > .group-column-close");
-  var groupExpanded = false;
 
   /**
    * Method used to expand the selected group column to full available width
@@ -32,6 +34,37 @@ export const GroupsController = function () {
       groupCloseButton.classList.remove("show");
       groupExpanded = false;
     }
+  };
+
+  /**
+   * Method used to receive random element from provided array
+   * @param {Object} array The array from which we want to receive element
+   * @param {Boolean} remove If we want to remove the received random element from array (to prevent duplicates)
+   * @returns random element from input array
+   */
+  const getRandom = function (array, remove) {
+    const randomItem = array[Math.floor(Math.random() * array.length)];
+    if (remove) {
+      var index = array.indexOf(randomItem);
+      if (index !== -1) {
+        array.splice(index, 1);
+      }
+    }
+    return randomItem;
+  };
+
+  /**
+   * Method used to set style for all group columns (position, colors and animation)
+   */
+  const initStyle = function () {
+    groupColumns.forEach((column) => {
+      // get color from array and then remove it so no duplicates are selected (in next iterations)
+      const selectedColor = getRandom(colors, true);
+      column.classList.add("background-" + selectedColor);
+      column.querySelector(".group-title").classList.add("background-" + selectedColor);
+      // get animation from array (duplicates are allowed in this case)
+      column.classList.add(getRandom(animations, false));
+    });
   };
 
   /**
@@ -83,6 +116,7 @@ export const GroupsController = function () {
    * Method used to setup the column controller
    */
   const setup = function () {
+    initStyle();
     initDimensions();
     bindListeners();
   };
