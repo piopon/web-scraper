abce8b4b1e3c36155f43908496e1cc16438835e6
Piotr Ponikowski <piopon.github@gmail.com>
2023-09-30 18:22:05 +0200

    added request options with PUT method and JSON content

--- public/js/observers-service.js
+++ public/js/observers-service.js
@@ -1,8 +1,20 @@
 export class ObserversService {
   static updateObserver(observerId) {
-    fetch(`api/v1/configs/groups`)
-      .then((response) => response.json())
+    fetch(`api/v1/configs/groups/observers?path=${observerId}`, this.#createSetRequestOptions("PUT"))
+      .then((response) => response.text())
       .then((data) => console.log(data))
       .catch((error) => console.log(error));
+  }
+
+  static #createObserver() {
+    return {};
+  }
+
+  static #createSetRequestOptions(method) {
+    return {
+      method: method,
+      headers: { "Content-Type": "application/json" },
+      body: this.#createObserver(),
+    };
   }
 }
