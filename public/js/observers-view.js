c28a0351104063c2a4dee854ad54983aef6e35e8
Piotr Ponikowski <piopon.github@gmail.com>
2023-10-16 18:38:07 +0200

    added core observer modal HTML method and used it in new and existing observer

--- public/js/observers-view.js
+++ public/js/observers-view.js
@@ -15,13 +15,28 @@ export class ObserversView {
    * @param {Object} observer The observer object which HTML code we want to get
    */
   static #getExistingObserverHtml(observer) {
-    console.log(observer);
+    return `<div class="observer-content">
+              ${ObserversView.#getObserverModalHtml(observer)}
+              <div class="modal-button">${observer.name}</div>
+            </div>`;
   }
 
   /**
    * Method used to receive HTML code representing new observer UI
    */
   static #getNewObserverHtml() {
-    console.log("new observer button");
+    return `<div class="observer-content">
+              ${ObserversView.#getObserverModalHtml(undefined)}
+              <div class="modal-button new-observer">+</div>
+            </div>`;
+  }
+
+  static #getObserverModalHtml(observer) {
+    return `<div class="modal-dialog hidden">
+              <div class="modal-background">
+                <div class="modal-content">
+                </div>
+              </div>
+            </div>`;
   }
 }
