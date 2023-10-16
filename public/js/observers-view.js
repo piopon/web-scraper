f8bd76e923219484e3b06ab8c6c50d216c8c5b82
Piotr Ponikowski <piopon.github@gmail.com>
2023-10-16 20:03:27 +0200

    fixed all observer HTML getters to handle new observer logic

--- public/js/observers-view.js
+++ public/js/observers-view.js
@@ -43,9 +43,9 @@ export class ObserversView {
                     ${ObserversView.#getObserverRootDataRow2Html(observer)}
                   </div>
                   <div class="component-cards">
-                    ${ObserversView.#getObserverTitleComponentHtml(observer.title)}
-                    ${ObserversView.#getObserverImageComponentHtml(observer.image)}
-                    ${ObserversView.#getObserverPriceComponentHtml(observer.price)}
+                    ${ObserversView.#getObserverTitleComponentHtml(observer !== undefined ? observer.title : undefined)}
+                    ${ObserversView.#getObserverImageComponentHtml(observer !== undefined ? observer.image : undefined)}
+                    ${ObserversView.#getObserverPriceComponentHtml(observer !== undefined ? observer.price : undefined)}
                   </div>
                 </div>
               </div>
@@ -53,78 +53,89 @@ export class ObserversView {
   }
 
   static #getObserverRootDataRow1Html(observer) {
+    const name = observer !== undefined ? observer.name : "";
+    const target = observer !== undefined ? observer.target : "";
+    const history = observer !== undefined ? observer.history : "";
     return `<div class="observer-data-row1">
               <div class="widget fill">
                 <label class="observer-label">name:</label>
                 <div class="id"></div>
-                <input type="text" class="observer-name" name="name" value="${observer.name}" />
+                <input type="text" class="observer-name" name="name" value="${name}" />
               </div>
               <div class="widget">
                 <label class="observer-label">target:</label>
                 <select class="observer-target" name="target" required>
-                  <option value="" disabled hidden>Select value</option>
-                  <option value=load ${component.target === "load" ? "selected" : ""}>load</option>
-                  <option value=domcontentloaded ${component.target === "domcontentloaded" ? "selected" : ""}>domcontentloaded</option>
-                  <option value=networkidle0 ${component.target === "networkidle0" ? "selected" : ""}>networkidle0</option>
-                  <option value=networkidle2 ${component.target === "networkidle2" ? "selected" : ""}>networkidle2</option>
+                  <option value="" disabled hidden ${target === "" ? "selected" : ""}>Select value</option>
+                  <option value=load ${target === "load" ? "selected" : ""}>load</option>
+                  <option value=domcontentloaded ${target === "domcontentloaded" ? "selected" : ""}>domcontentloaded</option>
+                  <option value=networkidle0 ${target === "networkidle0" ? "selected" : ""}>networkidle0</option>
+                  <option value=networkidle2 ${target === "networkidle2" ? "selected" : ""}>networkidle2</option>
                 </select>
               </div>
               <div class="widget">
                 <label class="observer-label">history:</label>
                 <select class="observer-history" name="history" required>
-                  <option value="" disabled hidden>Select value</option>
-                  <option value=off ${component.history === "off" ? "selected" : ""}>off</option>
-                  <option value=onChange ${component.history === "onChange" ? "selected" : ""}>onChange</option>
-                  <option value=on ${component.history === "on" ? "selected" : ""}>on</option>
+                  <option value="" disabled hidden ${history === "" ? "selected" : ""}>Select value</option>
+                  <option value=off ${history === "off" ? "selected" : ""}>off</option>
+                  <option value=onChange ${history === "onChange" ? "selected" : ""}>onChange</option>
+                  <option value=on ${history === "on" ? "selected" : ""}>on</option>
                 </select>
               </div>
             </div>`;
   }
 
   static #getObserverRootDataRow2Html(observer) {
+    const path = observer !== undefined ? observer.path : "";
+    const container = observer !== undefined ? observer.container : "";
     return `<div class="observer-data-row2">
               <div class="widget fill">
                 <label class="observer-label">path:</label>
-                <input type="text" class="observer-path" name="path" value="${observer.path}" />
+                <input type="text" class="observer-path" name="path" value="${path}" />
               </div>
               <div class="widget fill">
                 <label class="observer-label">container:</label>
-                <input type="text" class="observer-container" name="container" value="${observer.container}" />
+                <input type="text" class="observer-container" name="container" value="${container}" />
               </div>
             </div>`;
   }
 
   static #getObserverTitleComponentHtml(component) {
+    const selector = component !== undefined ? component.selector : "";
+    const attribute = component !== undefined ? component.attribute : "";
+    const auxiliary = component !== undefined ? component.auxiliary : "";
     return `<div class="component-card" active>
               <h3 class="card-title">title config</h3>
               <div class="component-content">
                 <div class="widget">
                   <label class="component-title-label">selector:</label>
-                  <input type="text" class="component-title-selector" name="selector" value="${component.selector}" />
+                  <input type="text" class="component-title-selector" name="selector" value="${selector}" />
                 </div>
                 <div class="widget">
                   <label class="component-title-label">attribute:</label>
-                  <input type="text" class="component-title-attribute" name="attribute" value="${component.attribute}" />
+                  <input type="text" class="component-title-attribute" name="attribute" value="${attribute}" />
                 </div>
                 <div class="widget">
                   <label class="component-title-label">auxiliary:</label>
-                  <input type="text" class="component-title-auxiliary" name="auxiliary" value="${component.auxiliary}" />
+                  <input type="text" class="component-title-auxiliary" name="auxiliary" value="${auxiliary}" />
                 </div>
               </div>
             </div>`;
   }
 
   static #getObserverImageComponentHtml(component) {
-    return `<div class="component-card" >
+    const selector = component !== undefined ? component.selector : "";
+    const attribute = component !== undefined ? component.attribute : "";
+    const auxiliary = component !== undefined ? component.auxiliary : "";
+    return `<div class="component-card">
               <h3 class="card-title">image config</h3>
               <div class="component-content">
                 <div class="widget">
                   <label class="component-image-label">selector:</label>
-                  <input type="text" class="component-image-selector" name="selector" value="${component.selector}" />
+                  <input type="text" class="component-image-selector" name="selector" value="${selector}" />
                 </div>
                 <div class="widget">
                   <label class="component-image-label">attribute:</label>
-                  <input type="text" class="component-image-attribute" name="attribute" value="${component.attribute}" />
+                  <input type="text" class="component-image-attribute" name="attribute" value="${attribute}" />
                 </div>
                 <div class="widget">
                   <label class="component-image-label">auxiliary:</label>
@@ -135,25 +146,28 @@ export class ObserversView {
   }
 
   static #getObserverPriceComponentHtml(component) {
-    return `<div class="component-card" >
+    const selector = component !== undefined ? component.selector : "";
+    const attribute = component !== undefined ? component.attribute : "";
+    const auxiliary = component !== undefined ? component.auxiliary : "";
+    return `<div class="component-card">
               <h3 class="card-title">price config</h3>
               <div class="component-content">
                 <div class="widget">
                   <label class="component-price-label">selector:</label>
-                  <input type="text" class="component-price-selector" name="selector" value="${component.selector}" />
+                  <input type="text" class="component-price-selector" name="selector" value="${selector}" />
                 </div>
                 <div class="widget">
                   <label class="component-price-label">attribute:</label>
-                  <input type="text" class="component-price-attribute" name="attribute" value="${component.attribute}" />
+                  <input type="text" class="component-price-attribute" name="attribute" value="${attribute}" />
                 </div>
                 <div class="widget">
                   <label class="component-price-label">auxiliary:</label>
                   <select class="component-price-auxiliary" name="auxiliary" required>
-                    <option value="" disabled hidden>Select value</option>
-                    <option value=PLN ${component.price === "PLN" ? "selected" : ""}>PLN</option>
-                    <option value=GBP ${component.price === "GBP" ? "selected" : ""}>GBP</option>
-                    <option value=USD ${component.price === "USD" ? "selected" : ""}>USD</option>
-                    <option value=EUR ${component.price === "EUR" ? "selected" : ""}>EUR</option>
+                    <option value="" disabled hidden ${auxiliary === "" ? "selected" : ""}>Select value</option>
+                    <option value=PLN ${auxiliary === "PLN" ? "selected" : ""}>PLN</option>
+                    <option value=GBP ${auxiliary === "GBP" ? "selected" : ""}>GBP</option>
+                    <option value=USD ${auxiliary === "USD" ? "selected" : ""}>USD</option>
+                    <option value=EUR ${auxiliary === "EUR" ? "selected" : ""}>EUR</option>
                   </select>
                 </div>
               </div>
