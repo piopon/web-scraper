4456141d65cada1badd11046d2e50dd842aabe4c
Piotr Ponikowski <piopon.github@gmail.com>
2023-10-16 19:38:03 +0200

    added all observer components HTML getters

--- public/js/observers-view.js
+++ public/js/observers-view.js
@@ -41,4 +41,70 @@ export class ObserversView {
               </div>
             </div>`;
   }
+
+  static #getObserverTitleComponentHtml(component) {
+    return `<div class="component-card" active>
+              <h3 class="card-title">title config</h3>
+              <div class="component-content">
+                <div class="widget">
+                  <label class="component-title-label">selector:</label>
+                  <input type="text" class="component-title-selector" name="selector" value="${component.selector}" />
+                </div>
+                <div class="widget">
+                  <label class="component-title-label">attribute:</label>
+                  <input type="text" class="component-title-attribute" name="attribute" value="${component.attribute}" />
+                </div>
+                <div class="widget">
+                  <label class="component-title-label">auxiliary:</label>
+                  <input type="text" class="component-title-auxiliary" name="auxiliary" value="${component.auxiliary}" />
+                </div>
+              </div>
+            </div>`;
+  }
+
+  static #getObserverImageComponentHtml(component) {
+    return `<div class="component-card" >
+              <h3 class="card-title">image config</h3>
+              <div class="component-content">
+                <div class="widget">
+                  <label class="component-image-label">selector:</label>
+                  <input type="text" class="component-image-selector" name="selector" value="${component.selector}" />
+                </div>
+                <div class="widget">
+                  <label class="component-image-label">attribute:</label>
+                  <input type="text" class="component-image-attribute" name="attribute" value="${component.attribute}" />
+                </div>
+                <div class="widget">
+                  <label class="component-image-label">auxiliary:</label>
+                  <input type="button" class="component-image-auxiliary" name="auxiliary" value="Select image" />
+                </div>
+              </div>
+            </div>`;
+  }
+
+  static #getObserverPriceComponentHtml(component) {
+    return `<div class="component-card" >
+              <h3 class="card-title">price config</h3>
+              <div class="component-content">
+                <div class="widget">
+                  <label class="component-price-label">selector:</label>
+                  <input type="text" class="component-price-selector" name="selector" value="${component.selector}" />
+                </div>
+                <div class="widget">
+                  <label class="component-price-label">attribute:</label>
+                  <input type="text" class="component-price-attribute" name="attribute" value="${component.attribute}" />
+                </div>
+                <div class="widget">
+                  <label class="component-price-label">auxiliary:</label>
+                  <select class="component-price-auxiliary" name="auxiliary" required>
+                    <option value="" disabled hidden>Select value</option>
+                    <option value=PLN ${component.price === "PLN" ? "selected" : ""}>PLN</option>
+                    <option value=GBP ${component.price === "GBP" ? "selected" : ""}>GBP</option>
+                    <option value=USD ${component.price === "USD" ? "selected" : ""}>USD</option>
+                    <option value=EUR ${component.price === "EUR" ? "selected" : ""}>EUR</option>
+                  </select>
+                </div>
+              </div>
+            </div>`;
+  }
 }
