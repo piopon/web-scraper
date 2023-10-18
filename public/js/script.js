2500c075af54697b9596ee4f501be7c6f04aca2e
Piotr Ponikowski <piopon.github@gmail.com>
2023-10-18 20:02:10 +0200

    bugfix: adjusted main script.js to use component and group controllers like classes

--- public/js/script.js
+++ public/js/script.js
@@ -2,9 +2,6 @@ import { ComponentsController } from "./controller-component.js";
 import { ObserversController } from "./controller-observer.js";
 import { GroupsController } from "./controller-group.js";
 
-const componentsController = ComponentsController();
+const componentsController = new ComponentsController();
 const observersController = new ObserversController();
-const groupsController = GroupsController();
-
-componentsController.initialize();
-groupsController.initialize();
+const groupsController = new GroupsController();
