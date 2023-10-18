98bd70e8dab7c8b856e9e37530821989b15612c0
Piotr Ponikowski <piopon.github@gmail.com>
2023-10-18 20:17:30 +0200

    fixed encapsulation in group controller class

--- public/js/controller-group.js
+++ public/js/controller-group.js
@@ -6,109 +6,25 @@ export class GroupsController {
   #groupCloseButtons = document.querySelectorAll(".group-buttons > .group-close-btn");
 
   constructor() {
-    this.initStyle();
-    this.initDimensions();
-    this.bindListeners();
+    this.#initStyle();
+    this.#initDimensions();
+    this.#bindListeners();
   }
-
-  /**
-   * Method used to expand the selected group column to full available width
-   * @param {Object} groupColumn The column which should be expanded
-   */
-  expand(groupColumn) {
-    if (!this.#groupExpanded) {
-      // add group column will always display hint when expading - we must remove it
-      if ("add" === groupColumn.dataset.action) {
-        groupColumn.parentNode.classList.remove("show-hint");
-      }
-      // go with regular flow for all columns
-      this.#groupColumns.forEach((column) => {
-        column.parentNode.classList.add(column === groupColumn ? "expanded" : "collapsed");
-        this.clearDimension(column);
-      });
-      this.#groupExpanded = true;
-    }
-  };
-
-  /**
-   * Method used to collapse the selected group column to display all available choices/columns
-   * @param {Object} groupCloseButton The close button of column which should be collapsed
-   */
-  collapse(groupCloseButton) {
-    if (this.#groupExpanded) {
-      this.#groupColumns.forEach((column) => {
-        column.parentNode.classList.remove("expanded");
-        column.parentNode.classList.remove("collapsed");
-        this.setDimension(column);
-      });
-      groupCloseButton.classList.remove("show");
-      this.#groupExpanded = false;
-    }
-  };
-
-  /**
-   * Method used to show column hint (available only for "add" gruop column)
-   * @param {Object} column The column for which we want to show a hint
-   */
-  showHint(column) {
-    // hint is only available for add group column
-    if ("add" !== column.dataset.action) {
-      return;
-    }
-    // show hind and clear dimension only when new group is NOT expanded
-    if (!column.parentNode.classList.contains("expanded")) {
-      column.parentNode.classList.add("show-hint");
-      this.clearDimension(column);
-    }
-  };
-
-  /**
-   * Method used to hide column hint (available only for "add" gruop column)
-   * @param {Object} column The column for which we want to hide a hint
-   */
-  hideHint(column) {
-    // hint is only available for add group column
-    if ("add" !== column.dataset.action) {
-      return;
-    }
-    column.parentNode.classList.remove("show-hint");
-    // restore dimensions only when new group is NOT expanded
-    if (!column.parentNode.classList.contains("expanded")) {
-      this.setDimension(column);
-    }
-  };
-
-  /**
-   * Method used to receive random element from provided array
-   * @param {Object} array The array from which we want to receive element
-   * @param {Boolean} remove If we want to remove the received random element from array (to prevent duplicates)
-   * @returns random element from input array
-   */
-  getRandom(array, remove) {
-    const randomItem = array[Math.floor(Math.random() * array.length)];
-    if (remove) {
-      var index = array.indexOf(randomItem);
-      if (index !== -1) {
-        array.splice(index, 1);
-      }
-    }
-    return randomItem;
-  };
 
   /**
    * Method used to set style for all group columns (position, colors and animation)
    */
-  initStyle() {
+  #initStyle() {
     const colors = ["navy", "aqua", "green", "orange", "red", "blue", "yellow", "plum"];
     const animations = ["column-from-top", "column-from-right", "column-from-bottom", "column-from-left"];
     this.#groupColumns.forEach((column) => {
       if ("update" === column.dataset.action) {
         // get color from array and then remove it so no duplicates are selected (in next iterations)
-        const selectedColor = this.getRandom(colors, true);
+        const selectedColor = this.#getRandom(colors, true);
         column.classList.add("background-" + selectedColor);
         column.querySelector(".group-title").classList.add("background-" + selectedColor);
         // get animation from array (duplicates are allowed in this case)
-        column.classList.add(this.getRandom(animations, false));
+        column.classList.add(this.#getRandom(animations, false));
       } else {
         // new group column should always appear from right and have gray background
         column.classList.add("background-violet");
@@ -124,10 +40,111 @@ export class GroupsController {
   };
 
   /**
+   * Method used to initialize column dimensions (positions and size)
+   */
+  #initDimensions() {
+    this.#groupColumns.forEach((column) => this.#setDimension(column));
+  };
+
+  /**
+   * Method used to bind UI listeners to controller methods.
+   * This method handles: group column and close column buttons clicksss
+   */
+  #bindListeners() {
+    this.#groupColumns.forEach((column) => {
+      column.addEventListener("click", (event) => {
+        this.#expand(column);
+        event.stopPropagation();
+      });
+      column.addEventListener("mouseover", (event) => {
+        this.#showHint(column);
+        event.stopPropagation();
+      });
+      column.addEventListener("mouseout", (event) => {
+        this.#hideHint(column);
+        event.stopPropagation();
+      });
+    });
+    this.#groupCloseButtons.forEach((closeButton) => {
+      closeButton.addEventListener("click", (event) => {
+        this.#collapse(closeButton);
+        event.stopPropagation();
+      });
+    });
+  };
+
+  /**
+   * Method used to expand the selected group column to full available width
+   * @param {Object} groupColumn The column which should be expanded
+   */
+  #expand(groupColumn) {
+    if (!this.#groupExpanded) {
+      // add group column will always display hint when expading - we must remove it
+      if ("add" === groupColumn.dataset.action) {
+        groupColumn.parentNode.classList.remove("show-hint");
+      }
+      // go with regular flow for all columns
+      this.#groupColumns.forEach((column) => {
+        column.parentNode.classList.add(column === groupColumn ? "expanded" : "collapsed");
+        this.#clearDimension(column);
+      });
+      this.#groupExpanded = true;
+    }
+  };
+
+  /**
+   * Method used to collapse the selected group column to display all available choices/columns
+   * @param {Object} groupCloseButton The close button of column which should be collapsed
+   */
+  #collapse(groupCloseButton) {
+    if (this.#groupExpanded) {
+      this.#groupColumns.forEach((column) => {
+        column.parentNode.classList.remove("expanded");
+        column.parentNode.classList.remove("collapsed");
+        this.#setDimension(column);
+      });
+      groupCloseButton.classList.remove("show");
+      this.#groupExpanded = false;
+    }
+  };
+
+  /**
+   * Method used to show column hint (available only for "add" gruop column)
+   * @param {Object} column The column for which we want to show a hint
+   */
+  #showHint(column) {
+    // hint is only available for add group column
+    if ("add" !== column.dataset.action) {
+      return;
+    }
+    // show hind and clear dimension only when new group is NOT expanded
+    if (!column.parentNode.classList.contains("expanded")) {
+      column.parentNode.classList.add("show-hint");
+      this.#clearDimension(column);
+    }
+  };
+
+  /**
+   * Method used to hide column hint (available only for "add" gruop column)
+   * @param {Object} column The column for which we want to hide a hint
+   */
+  #hideHint(column) {
+    // hint is only available for add group column
+    if ("add" !== column.dataset.action) {
+      return;
+    }
+    column.parentNode.classList.remove("show-hint");
+    // restore dimensions only when new group is NOT expanded
+    if (!column.parentNode.classList.contains("expanded")) {
+      this.#setDimension(column);
+    }
+  };
+
+  /**
    * Method used to set the dimensions (position and width) of the specified column
    * @param {Object} column The column which dimensions should be configured
    */
-  setDimension(column) {
+  #setDimension(column) {
     const NEW_GROUP_COLUMN_WIDTH = 4;
     if ("update" === column.dataset.action) {
       const columnWidth = (100 - NEW_GROUP_COLUMN_WIDTH) / (this.#groupColumns.length - 1);
@@ -144,41 +161,24 @@ export class GroupsController {
    * Method used to clear the dimensions of the selected column
    * @param {Object} column The column which dimensions should be cleared
    */
-  clearDimension(column) {
+  #clearDimension(column) {
     column.parentNode.removeAttribute("style");
   };
 
   /**
-   * Method used to bind UI listeners to controller methods.
-   * This method handles: group column and close column buttons clicksss
+   * Method used to receive random element from provided array
+   * @param {Object} array The array from which we want to receive element
+   * @param {Boolean} remove If we want to remove the received random element from array (to prevent duplicates)
+   * @returns random element from input array
    */
-  bindListeners() {
-    this.#groupColumns.forEach((column) => {
-      column.addEventListener("click", (event) => {
-        this.expand(column);
-        event.stopPropagation();
-      });
-      column.addEventListener("mouseover", (event) => {
-        this.showHint(column);
-        event.stopPropagation();
-      });
-      column.addEventListener("mouseout", (event) => {
-        this.hideHint(column);
-        event.stopPropagation();
-      });
-    });
-    this.#groupCloseButtons.forEach((closeButton) => {
-      closeButton.addEventListener("click", (event) => {
-        this.collapse(closeButton);
-        event.stopPropagation();
-      });
-    });
-  };
-
-  /**
-   * Method used to initialize column dimensions (positions and size)
-   */
-  initDimensions() {
-    this.#groupColumns.forEach((column) => this.setDimension(column));
+  #getRandom(array, remove) {
+    const randomItem = array[Math.floor(Math.random() * array.length)];
+    if (remove) {
+      var index = array.indexOf(randomItem);
+      if (index !== -1) {
+        array.splice(index, 1);
+      }
+    }
+    return randomItem;
   };
 };
