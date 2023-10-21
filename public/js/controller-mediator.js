c9f310a135971416d37c437a2e7e4e54f4db9d3b
Piotr Ponikowski <piopon.github@gmail.com>
2023-10-21 17:28:15 +0200

    added observers mediator methods descriptions

--- public/js/controller-mediator.js
+++ public/js/controller-mediator.js
@@ -1,6 +1,10 @@
 export class ControllersMediator {
   #registeredClasses = [];
 
+  /**
+   * Method used to registed specified class to mediator
+   * @param {Object} classInstance The class which should be registered
+   */
   register(classInstance) {
     if (this.#checkInterface(classInstance)) {
       this.#registeredClasses.push(classInstance);
@@ -10,6 +14,12 @@ export class ControllersMediator {
     }
   }
 
+  /**
+   * The method used to notify all registered classes (except sender) that new event was transmitted
+   * @param {Object} sender The sender of the event
+   * @param {String} eventType The event type which should be propagated to all regsitered classes
+   * @param {Object} eventObject The event object sent with event
+   */
   notify(sender, eventType, eventObject) {
     for (let i = 0; i < this.#registeredClasses.length; i++) {
       if (this.#registeredClasses[i] !== sender) {
@@ -18,6 +28,11 @@ export class ControllersMediator {
     }
   }
 
+  /**
+   * The method used to check if specified obejct fullfills mediator needed interface
+   * @param {Object} classInstance The object which should be verified
+   * @returns true if specified object has proper interface, false otherwise
+   */
   #checkInterface(classInstance) {
     return "emitEvent" in classInstance && "handleEvent" in classInstance;
   }
