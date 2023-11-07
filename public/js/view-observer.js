9cdb072235bffde1315f25ac03dc5e1f163cb677
Piotr Ponikowski <piopon.github@gmail.com>
2023-11-07 23:33:48 +0100

    updated reloaded observer views including the newly added toggles

--- public/js/view-observer.js
+++ public/js/view-observer.js
@@ -140,17 +140,25 @@ export class ObserversView {
     return `<div class="component-card" active>
               <h3 class="card-title">title config</h3>
               <div class="component-content">
-                <div class="widget">
-                  <label class="component-title-label">selector:</label>
-                  <input type="text" class="component-title-selector" name="selector" value="${selector}" />
+                <div class="component-fields">
+                  <div class="widget">
+                    <label class="component-title-label">selector:</label>
+                    <input type="text" class="component-title-selector" name="selector" value="${selector}" />
+                  </div>
+                  <div class="widget">
+                    <label class="component-title-label">attribute:</label>
+                    <input type="text" class="component-title-attribute" name="attribute" value="${attribute}" />
+                  </div>
+                  <div class="widget">
+                    <label class="component-title-label">auxiliary:</label>
+                    <input type="text" class="component-title-auxiliary" name="auxiliary" value="${auxiliary}" />
+                  </div>
                 </div>
-                <div class="widget">
-                  <label class="component-title-label">attribute:</label>
-                  <input type="text" class="component-title-attribute" name="attribute" value="${attribute}" />
-                </div>
-                <div class="widget">
-                  <label class="component-title-label">auxiliary:</label>
-                  <input type="text" class="component-title-auxiliary" name="auxiliary" value="${auxiliary}" />
+                <div class="component-toggle">
+                  <p>auto</p>
+                  <input type="checkbox" class="check-auto-manual">
+                  <label for="check-auto-manual"></label>
+                  <p>manual</p>
                 </div>
               </div>
             </div>`;
@@ -168,17 +176,25 @@ export class ObserversView {
     return `<div class="component-card">
               <h3 class="card-title">image config</h3>
               <div class="component-content">
-                <div class="widget">
-                  <label class="component-image-label">selector:</label>
-                  <input type="text" class="component-image-selector" name="selector" value="${selector}" />
+                <div class="component-fields">
+                  <div class="widget">
+                    <label class="component-image-label">selector:</label>
+                    <input type="text" class="component-image-selector" name="selector" value="${selector}" />
+                  </div>
+                  <div class="widget">
+                    <label class="component-image-label">attribute:</label>
+                    <input type="text" class="component-image-attribute" name="attribute" value="${attribute}" />
+                  </div>
+                  <div class="widget">
+                    <label class="component-image-label">auxiliary:</label>
+                    <input type="button" class="component-image-auxiliary" name="auxiliary" value="Select image" />
+                  </div>
                 </div>
-                <div class="widget">
-                  <label class="component-image-label">attribute:</label>
-                  <input type="text" class="component-image-attribute" name="attribute" value="${attribute}" />
-                </div>
-                <div class="widget">
-                  <label class="component-image-label">auxiliary:</label>
-                  <input type="button" class="component-image-auxiliary" name="auxiliary" value="Select image" />
+                <div class="component-toggle">
+                  <p>auto</p>
+                  <input type="checkbox" class="check-auto-manual">
+                  <label for="check-auto-manual"></label>
+                  <p>manual</p>
                 </div>
               </div>
             </div>`;
@@ -196,23 +212,25 @@ export class ObserversView {
     return `<div class="component-card">
               <h3 class="card-title">price config</h3>
               <div class="component-content">
-                <div class="widget">
-                  <label class="component-price-label">selector:</label>
-                  <input type="text" class="component-price-selector" name="selector" value="${selector}" />
-                </div>
-                <div class="widget">
-                  <label class="component-price-label">attribute:</label>
-                  <input type="text" class="component-price-attribute" name="attribute" value="${attribute}" />
-                </div>
-                <div class="widget">
-                  <label class="component-price-label">auxiliary:</label>
-                  <select class="component-price-auxiliary" name="auxiliary" required>
-                    <option value="" disabled hidden ${auxiliary === "" ? "selected" : ""}>Select value</option>
-                    <option value=PLN ${auxiliary === "PLN" ? "selected" : ""}>PLN</option>
-                    <option value=GBP ${auxiliary === "GBP" ? "selected" : ""}>GBP</option>
-                    <option value=USD ${auxiliary === "USD" ? "selected" : ""}>USD</option>
-                    <option value=EUR ${auxiliary === "EUR" ? "selected" : ""}>EUR</option>
-                  </select>
+                <div class="component-fields">
+                  <div class="widget">
+                    <label class="component-price-label">selector:</label>
+                    <input type="text" class="component-price-selector" name="selector" value="${selector}" />
+                  </div>
+                  <div class="widget">
+                    <label class="component-price-label">attribute:</label>
+                    <input type="text" class="component-price-attribute" name="attribute" value="${attribute}" />
+                  </div>
+                  <div class="widget">
+                    <label class="component-price-label">auxiliary:</label>
+                    <select class="component-price-auxiliary" name="auxiliary" required>
+                      <option value="" disabled hidden ${auxiliary === "" ? "selected" : ""}>Select value</option>
+                      <option value=PLN ${auxiliary === "PLN" ? "selected" : ""}>PLN</option>
+                      <option value=GBP ${auxiliary === "GBP" ? "selected" : ""}>GBP</option>
+                      <option value=USD ${auxiliary === "USD" ? "selected" : ""}>USD</option>
+                      <option value=EUR ${auxiliary === "EUR" ? "selected" : ""}>EUR</option>
+                    </select>
+                  </div>
                 </div>
               </div>
             </div>`;
