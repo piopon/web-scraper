d714b1dcf6c99434b8a3a0e9c363a03fd6880055
Piotr Ponikowski <piopon.github@gmail.com>
2023-12-28 18:16:25 +0100

    added and used new is init needed method to avoid errors

--- public/js/script.js
+++ public/js/script.js
@@ -3,18 +3,28 @@ import { ComponentsController } from "./controller-component.js";
 import { ObserversController } from "./controller-observer.js";
 import { GroupsController } from "./controller-group.js";
 
-// persist backend variables in frontend local storage at reload
-storeInitialBackendValues();
-// creating mediator class transmitting events between controllers
-const mediator = new ControllersMediator();
-// creating controllers objects
-const componentsController = new ComponentsController();
-const observersController = new ObserversController();
-const groupsController = new GroupsController();
-// registering all controllers in mediator
-mediator.register(componentsController);
-mediator.register(observersController);
-mediator.register(groupsController);
+if (isGroupsInitializationNeeded()) {
+  // persist backend variables in frontend local storage at reload
+  storeInitialBackendValues();
+  // creating mediator class transmitting events between controllers
+  const mediator = new ControllersMediator();
+  // creating controllers objects
+  const componentsController = new ComponentsController();
+  const observersController = new ObserversController();
+  const groupsController = new GroupsController();
+  // registering all controllers in mediator
+  mediator.register(componentsController);
+  mediator.register(observersController);
+  mediator.register(groupsController);
+}
+
+/**
+ * Method used to check if scrap groups init is needed (happens only when user opened index page)
+ * @returns true when root index is opened and initialization is needed, false otherwise
+ */
+function isGroupsInitializationNeeded() {
+  return document.querySelector("section.group-columns") != null;
+}
 
 /**
  * Method used to store initial backend values to session storage
