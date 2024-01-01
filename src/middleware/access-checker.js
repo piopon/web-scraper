50feaba6ef3cae7b73f550b0107ddd684fd1b2d0
Piotr Ponikowski <piopon.github@gmail.com>
2024-01-02 00:01:31 +0100

    added new method to check if request is eligible for accessing user session views

--- src/middleware/access-checker.js
+++ src/middleware/access-checker.js
@@ -11,4 +11,17 @@ export class AccessChecker {
     }
     response.redirect("/login");
   }
+
+  /**
+   * Method used to check if the current request is eligible to access user session pages (login, register, etc.)
+   * @param {Object} request The incoming request object
+   * @param {Object} response The outputted response object
+   * @param {Function} next The next middleware function in the cycle
+   */
+  static canViewSessionUser(request, response, next) {
+    if (!request.isAuthenticated()) {
+      return next();
+    }
+    response.redirect("/");
+  }
 }
