06c9ecb7a3604d9a9ee6616696a90cf653b755be
Piotr Ponikowski <piopon.github@gmail.com>
2024-01-11 00:23:46 +0100

    added web db component start and stop methods with DB connect/disconnect calls

--- src/components/web-database.js
+++ src/components/web-database.js
@@ -1,10 +1,22 @@
+import mongoose from "mongoose";
+
 export class WebDatabase {
   static #COMPONENT_NAME = "web-db";
 
   #status = undefined;
+  #dbConfig = undefined;
 
   constructor(config) {
+    this.#dbConfig = config.databaseConfig;
     this.#status = new StatusLogger(WebDatabase.#COMPONENT_NAME, config.minLogLevel);
     this.#status.info("Created");
+  }
+
+  async start() {
+    mongoose.connect(`mongodb://${this.#dbConfig.url}:${this.#dbConfig.port}/${this.#dbConfig.name}`);
+  }
+
+  async stop() {
+    mongoose.disconnect();
   }
 }
