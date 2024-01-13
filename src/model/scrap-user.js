ffe96e427391a51690146c1e4d01da1bbbf00820
Piotr Ponikowski <piopon.github@gmail.com>
2024-01-14 00:53:10 +0100

    added method descriptions to newly added scrap-user.js

--- src/model/scrap-user.js
+++ src/model/scrap-user.js
@@ -3,10 +3,18 @@ import mongoose from "mongoose";
 export class ScrapUser {
   static #DATABASE_MODEL = mongoose.model("scraper-user", ScrapUser.#getDatabaseSchema());
 
+  /**
+   * Method used to receive the DB model of the scraper user object
+   * @returns database model object
+   */
   static getDatabaseModel() {
     return ScrapUser.#DATABASE_MODEL;
   }
 
+  /**
+   * Method used to receive the DB schema of the scraper user object
+   * @returns database schema object
+   */
   static #getDatabaseSchema() {
     return {
       name: String,
