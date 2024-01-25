0de9f89d4316805d6500baea6a79c8d19299e2a4
Piotr Ponikowski <piopon.github@gmail.com>
2024-01-26 00:03:08 +0100

    reformatted application types file

--- config/app-types.js
+++ config/app-types.js
@@ -3,18 +3,18 @@
  * It implements an enum with values: ERROR, WARNING, INFO, DEBUG
  */
 class LogLevel {
-    static ERROR = new LogLevel(0);
-    static WARNING = new LogLevel(1);
-    static INFO = new LogLevel(2);
-    static DEBUG = new LogLevel(3);
+  static ERROR = new LogLevel(0);
+  static WARNING = new LogLevel(1);
+  static INFO = new LogLevel(2);
+  static DEBUG = new LogLevel(3);
 
-    /**
-     * Creates an object representing log level setting
-     * @param {Number} value The log level integer value
-     */
-    constructor(value) {
-        this.value = value
-    }
+  /**
+   * Creates an object representing log level setting
+   * @param {Number} value The log level integer value
+   */
+  constructor(value) {
+    this.value = value;
+  }
 }
 
 /**
@@ -22,16 +22,16 @@ class LogLevel {
  * It implements an enum with values: INIT, LOGIN
  */
 class ComponentType {
-    static INIT = new ComponentType("init")
-    static LOGIN = new ComponentType("login")
+  static INIT = new ComponentType("init");
+  static LOGIN = new ComponentType("login");
 
-    /**
-     * Creates an object representing component type setting
-     * @param {String} name The component type name value
-     */
-    constructor(name) {
-      this.name = name
-    }
+  /**
+   * Creates an object representing component type setting
+   * @param {String} name The component type name value
+   */
+  constructor(name) {
+    this.name = name;
+  }
 }
 
-export { LogLevel, ComponentType }
+export { LogLevel, ComponentType };
