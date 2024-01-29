f45b9da5114b4b03a194318a371d29db57c81186
Piotr Ponikowski <piopon.github@gmail.com>
2024-01-29 22:43:49 +0100

    updated server's components init by passing components not objects

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { AppConfig } from "../config/app-config.js";
-import { ComponentType } from "../config/app-types.js";
 import { WebDatabase } from "./components/web-database.js";
 import { WebScraper } from "./components/web-scraper.js";
 import { WebServer } from "./components/web-server.js";
@@ -11,8 +10,8 @@ const webDatabase = new WebDatabase(config);
 const webScraper = new WebScraper(config);
 // create and start webserver
 const webServer = new WebServer(config);
-webServer.addComponent({ type: ComponentType.INIT, mustPass: false, item: webDatabase });
-webServer.addComponent({ type: ComponentType.LOGIN, mustPass: true, item: webScraper });
+webServer.addComponent(webDatabase);
+webServer.addComponent(webScraper);
 webServer.run();
 
 process.on("SIGTERM", () => webServer.shutdown());
