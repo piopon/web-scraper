988632e91a429e48670090a5e13e74eaf6ec0e32
Piotr Ponikowski <piopon.github@gmail.com>
2024-01-30 22:08:02 +0100

    added method to configure passport serialize and deserialize user logic

--- src/routes/view/auth-router.js
+++ src/routes/view/auth-router.js
@@ -1,14 +1,33 @@
-export class AuthRouter {
-    #components = [];
-    #passport = undefined;
+import { ScrapUser } from "../../model/scrap-user.js";
 
-    /**
-     * Creates a new auth router for managing user authentication and authorization
-     * @param {Array} components The components list used in auth process (LOGIN)
-     * @param {Object} passport The object controlling user sing-up and sing-in process
-     */
-    constructor(components, passport) {
-      this.#components = components;
-      this.#passport = passport;
+export class AuthRouter {
+  #components = [];
+  #passport = undefined;
+
+  /**
+   * Creates a new auth router for managing user authentication and authorization
+   * @param {Array} components The components list used in auth process (LOGIN)
+   * @param {Object} passport The object controlling user sing-up and sing-in process
+   */
+  constructor(components, passport) {
+    this.#components = components;
+    this.#passport = passport;
+    this.#configAuthenitcation(passport);
+  }
+
+  /**
+   * Method used to configure Passport authentication object
+   * @param {Object} passport The auth object to be configured
+   */
+  #configAuthenitcation(passport) {
+    // configure common serialize and deserialize user logic
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+    passport.deserializeUser(async (id, done) => {
+      const user = await ScrapUser.getDatabaseModel().findById(id);
+      done(null, user);
+    });
+  }
     }
 }
