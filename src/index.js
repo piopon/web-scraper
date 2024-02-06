516be7a08b487e53c1d27ec88503c5a275687373
Piotr Ponikowski <piopon.github@gmail.com>
2024-02-07 00:06:35 +0100

    added top-level try catch block to handle ALL exceptions correctly

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import { WebDatabase } from "./components/web-database.js";
 import { WebScraper } from "./components/web-scraper.js";
 import { WebServer } from "./components/web-server.js";
 
-// initialize application config
-const config = new AppConfig().getConfig();
-// create dependent components
-const webDatabase = new WebDatabase(config);
-const webScraper = new WebScraper(config);
-// create and start webserver
-const webServer = new WebServer(config);
-webServer.addComponent(webDatabase);
-webServer.addComponent(webScraper);
-webServer.run();
-
-process.on("SIGTERM", () => webServer.shutdown());
+try {
+  // initialize application config
+  const config = new AppConfig().getConfig();
+  // create dependent components
+  const webDatabase = new WebDatabase(config);
+  const webScraper = new WebScraper(config);
+  // create and start webserver
+  const webServer = new WebServer(config);
+  webServer.addComponent(webDatabase);
+  webServer.addComponent(webScraper);
+  webServer.run();
+  // handle user termination signal
+  process.on("SIGTERM", () => webServer.shutdown());
+} catch (exception) {
+  console.error(exception.message);
+}
