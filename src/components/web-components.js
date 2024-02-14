d095de071157b76440a3627b493d5a8563a33de5
Piotr Ponikowski <piopon.github@gmail.com>
2024-02-14 23:19:46 +0100

    added method to get components of specified type

--- src/components/web-components.js
+++ src/components/web-components.js
@@ -4,4 +4,15 @@ export class WebComponents {
     addComponent(component) {
         this.#components.push(component);
     }
+
+  /**
+   * Method used to filter all components and return the one with desired type
+   * @param {Object} type The type of components that we want to receive
+   * @returns array of components of specified type
+   */
+  getComponents(type) {
+    return this.#components.filter((component) => {
+      return -1 !== component.master.getInfo().types.findIndex((currType) => currType.equals(type));
+    });
+  }
 }
