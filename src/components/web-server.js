b6e44af94e282e099dc0fc125a220723188ebd24
Piotr Ponikowski <piopon.github@gmail.com>
2024-02-14 23:47:42 +0100

    removed not needed component-related methods from WebServer class

--- src/components/web-server.js
+++ src/components/web-server.js
@@ -34,36 +34,6 @@ export class WebServer {
   }
 
   /**
-   * Method used to add a component to start after running web server
-   * @param {Object} component The component to start after running web server
-   */
-  addComponent(component) {
-    const componentTypes = component.getInfo().types;
-    if (componentTypes.length === 0) {
-      this.#status.warning(`Missing component type(s): ${component}`);
-    }
-    const componentMethods = Object.getOwnPropertyNames(Object.getPrototypeOf(component));
-    for (const componentType of componentTypes) {
-      const requiredMethods = componentType.methods;
-      if (!requiredMethods.every((method) => componentMethods.includes(method))) {
-        this.#status.error(`Incompatible component: ${component.getName()}`);
-        throw new Error("Cannot initialize server. Check previous logs for more information.");
-      }
-    }
-    if (componentTypes.includes(ComponentType.SLAVE)) {
-      const newMaster = component.getMaster();
-      const master = this.#components.find((c) => c.master.getName() === newMaster.name);
-      if (master != null) {
-        master.slave = component;
-      }
-      if (1 === componentTypes.length) {
-        return;
-      }
-    }
-    this.#components.push({ master: component, slave: undefined });
-  }
-
-  /**
    * Method used to initialize and run the web server
    */
   async run() {
@@ -133,47 +103,5 @@ export class WebServer {
       resave: false,
       saveUninitialized: false,
     };
-  }
-
-  /**
-   * Method used to filter all components and return the one with desired type
-   * @param {Object} type The type of components that we want to receive
-   * @returns array of components of specified type
-   */
-  #getComponents(type) {
-    return this.#components.filter((component) => {
-      return -1 !== component.master.getInfo().types.findIndex((currType) => currType.equals(type));
-    });
-  }
-
-  /**
-   * Method used to initialize and start server-related components
-   * @returns true if all components are invoked, false if at least one has an error
-   */
-  async #runComponents() {
-    const initComponents = this.#getComponents(ComponentType.INIT);
-    for (const component of initComponents) {
-      // if we don't wait for component initialization then start it and go to the next one
-      if (!component.master.getInfo().initWait) {
-        component.master.start().then(async (initialized) => {
-          // if component is initialized and has slave then run after initialization action
-          if (initialized && component.slave != null) {
-            component.slave.getMaster().actions.afterInit();
-          }
-        });
-        continue;
-      }
-      // we must wait for component initialization so we wait for the result and check it
-      const result = await component.master.start();
-      if (!result) {
-        this.#status.error(`Cannot start component: ${component.getName()}`);
-        return false;
-      }
-      // call the dependent component (if there is one)
-      if (component.slave != null) {
-        await component.slave.getMaster().actions.afterInit();
-      }
-    }
-    return true;
   }
 }
