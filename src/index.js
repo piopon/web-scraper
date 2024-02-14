41e35774b0f968322960c604b5f19aad8b4e42a9
Piotr Ponikowski <piopon.github@gmail.com>
2024-02-14 23:51:23 +0100

    updated main index.js logic to use new WebComponents class instance

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { AppConfig } from "../config/app-config.js";
+import { WebComponents } from "./components/web-components.js";
 import { WebDatabase } from "./components/web-database.js";
 import { WebScraper } from "./components/web-scraper.js";
 import { WebServer } from "./components/web-server.js";
@@ -7,12 +8,13 @@ try {
   // initialize application config
   const config = new AppConfig().getConfig();
   // create dependent components
+  const webComponents = new WebComponents(config);
   const webDatabase = new WebDatabase(config);
   const webScraper = new WebScraper(config);
+  webComponents.addComponent(webDatabase);
+  webComponents.addComponent(webScraper);
   // create and start webserver
-  const webServer = new WebServer(config);
-  webServer.addComponent(webDatabase);
-  webServer.addComponent(webScraper);
+  const webServer = new WebServer(config, webComponents);
   webServer.run();
   // handle user termination signal
   process.on("SIGTERM", () => webServer.shutdown());
