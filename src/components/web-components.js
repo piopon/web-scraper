24b07835388cb47443c28ddf7bcae3839daa5e53
Piotr Ponikowski <piopon.github@gmail.com>
2024-02-14 23:41:14 +0100

    added undefined default value for getComponent to return all components

--- src/components/web-components.js
+++ src/components/web-components.js
@@ -7,10 +7,13 @@ export class WebComponents {
 
   /**
    * Method used to filter all components and return the one with desired type
-   * @param {Object} type The type of components that we want to receive
+   * @param {Object} type The type of components that we want to receive (or undefined for all components)
    * @returns array of components of specified type
    */
-  getComponents(type) {
+  getComponents(type = undefined) {
+    if (undefined === type) {
+      return this.#components;
+    }
     return this.#components.filter((component) => {
       return -1 !== component.master.getInfo().types.findIndex((currType) => currType.equals(type));
     });
