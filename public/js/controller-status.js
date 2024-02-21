6584007bf724d116c59dc9dc62ecd1bbe82c44b3
Piotr Ponikowski <piopon.github@gmail.com>
2024-02-22 00:16:05 +0100

    added try-catch block in updateStatusIcon logic to handle server errors

--- public/js/controller-status.js
+++ public/js/controller-status.js
@@ -22,31 +22,35 @@ export class StatusController {
   }
 
   async #updateStatusIcons() {
-    const componentsStatus = await StatusService.getStatus();
-    for (const component of componentsStatus) {
-      const componentIcon = document.querySelector(`footer#main-footer i#status-${component.name.trim()}`);
-      switch (component.status) {
-        case "stopped":
-          componentIcon.classList.add("status-stopped");
-          componentIcon.classList.remove("status-initializing");
-          componentIcon.classList.remove("status-running");
-          break;
-        case "initializing":
-          componentIcon.classList.remove("status-stopped");
-          componentIcon.classList.add("status-initializing");
-          componentIcon.classList.remove("status-running");
-          break;
-        case "running":
-          componentIcon.classList.remove("status-stopped");
-          componentIcon.classList.remove("status-initializing");
-          componentIcon.classList.add("status-running");
-          break;
-        default:
-          componentIcon.classList.remove("status-stopped");
-          componentIcon.classList.remove("status-initializing");
-          componentIcon.classList.remove("status-running");
-          break;
+    try {
+      const componentsStatus = await StatusService.getStatus();
+      for (const component of componentsStatus) {
+        const componentIcon = document.querySelector(`footer#main-footer i#status-${component.name.trim()}`);
+        switch (component.status) {
+          case "stopped":
+            componentIcon.classList.add("status-stopped");
+            componentIcon.classList.remove("status-initializing");
+            componentIcon.classList.remove("status-running");
+            break;
+          case "initializing":
+            componentIcon.classList.remove("status-stopped");
+            componentIcon.classList.add("status-initializing");
+            componentIcon.classList.remove("status-running");
+            break;
+          case "running":
+            componentIcon.classList.remove("status-stopped");
+            componentIcon.classList.remove("status-initializing");
+            componentIcon.classList.add("status-running");
+            break;
+          default:
+            componentIcon.classList.remove("status-stopped");
+            componentIcon.classList.remove("status-initializing");
+            componentIcon.classList.remove("status-running");
+            break;
+        }
       }
+    } catch (error) {
+      console.logc("Cannot receive status data.")
     }
   }
 }
