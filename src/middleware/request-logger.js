4221fcde6c03520ae05b231f1a9dddfd77341569
Piotr Ponikowski <piopon.github@gmail.com>
2024-02-26 22:32:01 +0100

    changed request logger to use DEBUG logs

--- src/middleware/request-logger.js
+++ src/middleware/request-logger.js
@@ -7,7 +7,7 @@ export class RequestLogger {
   static middleware(logger) {
     return (request, response, next) => {
       const url = `${request.protocol}://${request.get("host")}${request.originalUrl}`;
-      logger.info(`${request.method} request - ${url}`);
+      logger.debug(`${request.method} request - ${url}`);
       next();
     };
   }
