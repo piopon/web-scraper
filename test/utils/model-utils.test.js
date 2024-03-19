f5bf4acd1cf02982c179f3434981eb3bf56b1c4a
Piotr Ponikowski <piopon.github@gmail.com>
2024-03-19 22:55:10 +0100

    added false case for isEmpty() method

--- test/utils/model-utils.test.js
+++ test/utils/model-utils.test.js
@@ -4,4 +4,7 @@ describe("isEmpty", () => {
   test("returns true if input object is empty", () => {
     expect(ModelUtils.isEmpty({})).toBe(true);
   });
+  test("returns false if input object is not empty", () => {
+    expect(ModelUtils.isEmpty({test: 1})).toBe(false);
+  });
 });
