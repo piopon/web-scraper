8ff00477c48a6fff2e9d8b9cb45d23e932084e5c
Piotr Ponikowski <piopon.github@gmail.com>
2024-03-20 00:03:07 +0100

    reorganized getValueOrDefault unit tests

--- test/utils/model-utils.test.js
+++ test/utils/model-utils.test.js
@@ -16,22 +16,28 @@ describe("isEmpty", () => {
 });
 
 describe("getValueOrDefault", () => {
-  test("returns integer value if input is present", () => {
-    expect(ModelUtils.getValueOrDefault(123, 0)).toBe(123);
+  describe("when using valid input value", () => {
+    test("returns correct integer", () => {
+      expect(ModelUtils.getValueOrDefault(123, 0)).toBe(123);
+    });
+    test("returns correct object", () => {
+      expect(ModelUtils.getValueOrDefault({prop: 123}, {default: 0})).toStrictEqual({prop: 123});
+    });
   });
-  test("returns default integer if input is undefined", () => {
-    expect(ModelUtils.getValueOrDefault(undefined, 0)).toBe(0);
+  describe("when using undefined input value", () => {
+    test("returns default integer", () => {
+      expect(ModelUtils.getValueOrDefault(undefined, 0)).toBe(0);
+    });
+    test("returns default object", () => {
+      expect(ModelUtils.getValueOrDefault(undefined, {default: 0})).toStrictEqual({default: 0});
+    });
   });
-  test("returns default integer if input is null", () => {
-    expect(ModelUtils.getValueOrDefault(null, 0)).toBe(0);
-  });
-  test("returns object value if input is present", () => {
-    expect(ModelUtils.getValueOrDefault({prop: 123}, {default: 0})).toStrictEqual({prop: 123});
-  });
-  test("returns default object if input is undefined", () => {
-    expect(ModelUtils.getValueOrDefault(undefined, {default: 0})).toStrictEqual({default: 0});
-  });
-  test("returns default object if input is null", () => {
-    expect(ModelUtils.getValueOrDefault(null, {default: 0})).toStrictEqual({default: 0});
+  describe("when using null input value", () => {
+    test("returns default integer", () => {
+      expect(ModelUtils.getValueOrDefault(null, 0)).toBe(0);
+    });
+    test("returns default object", () => {
+      expect(ModelUtils.getValueOrDefault(null, {default: 0})).toStrictEqual({default: 0});
+    });
   });
 });
