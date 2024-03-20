a9b9a1c3b9088d7ebfbb2d5fe926e60273a8cc54
Piotr Ponikowski <piopon.github@gmail.com>
2024-03-21 00:26:16 +0100

    cleanup already created regex utils tests

--- test/utils/regex-utils.test.js
+++ test/utils/regex-utils.test.js
@@ -2,18 +2,28 @@ import { RegexUtils } from "../../src/utils/regex-utils.js";
 
 describe("getPrices", () => {
   test("returns correct price with extra suffix", () => {
-    expect(RegexUtils.getPrices("12.33 PLN")).toStrictEqual(["12.33"]);
+    const input = "12.33 PLN";
+    const result = ["12.33"];
+    expect(RegexUtils.getPrices(input)).toStrictEqual(result);
   });
   test("returns correct price with extra prefix", () => {
-    expect(RegexUtils.getPrices("$3.567")).toStrictEqual(["3.567"]);
+    const input = "$3.567";
+    const result = ["3.567"];
+    expect(RegexUtils.getPrices(input)).toStrictEqual(result);
   });
   test("returns correct price with extra suffix and prefix", () => {
-    expect(RegexUtils.getPrices("PRICE: 1.99 PLN")).toStrictEqual(["1.99"]);
+    const input = "PRICE: 1.99 PLN";
+    const result = ["1.99"];
+    expect(RegexUtils.getPrices(input)).toStrictEqual(result);
   });
   test("returns correct price with comma", () => {
-    expect(RegexUtils.getPrices("39,99")).toStrictEqual(["39.99"]);
+    const input = "39,99";
+    const result = ["39.99"];
+    expect(RegexUtils.getPrices(input)).toStrictEqual(result);
   });
   test("returns correct price with multiple values", () => {
-    expect(RegexUtils.getPrices("PRICE: $39.99 | 43,99PLN | EUR41.99")).toStrictEqual(["39.99", "43.99", "41.99"]);
+    const input = "PRICE: $39.99 | 43,99PLN | EUR41.99";
+    const result = ["39.99", "43.99", "41.99"];
+    expect(RegexUtils.getPrices(input)).toStrictEqual(result);
   });
 });
