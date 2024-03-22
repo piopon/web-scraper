ebf864dd76286c4f53a8f07a55e0d77bce460048
Piotr Ponikowski <piopon.github@gmail.com>
2024-03-22 22:54:38 +0100

    new unit tests for different variants of component non-empty fields

--- test/model/scrap-component.test.js
+++ test/model/scrap-component.test.js
@@ -18,5 +18,21 @@ describe("getIdentifier", () => {
       const component = new ScrapComponent({ auxiliary: "auxiliary" });
       expect(component.getIdentifier()).toBe("component = empty | empty | auxiliary");
     });
+    test("with non-empty selector and attribute", () => {
+      const component = new ScrapComponent({ selector: "selector", attribute: "attribute" });
+      expect(component.getIdentifier()).toBe("component = selector | attribute | empty");
+    });
+    test("with non-empty selector and auxiliary", () => {
+      const component = new ScrapComponent({ selector: "selector", auxiliary: "auxiliary" });
+      expect(component.getIdentifier()).toBe("component = selector | empty | auxiliary");
+    });
+    test("with non-empty attribute and auxiliary", () => {
+      const component = new ScrapComponent({ attribute: "attribute", auxiliary: "auxiliary" });
+      expect(component.getIdentifier()).toBe("component = empty | attribute | auxiliary");
+    });
+    test("with all non-empty fields", () => {
+      const component = new ScrapComponent({ selector: "selector", attribute: "attribute", auxiliary: "auxiliary" });
+      expect(component.getIdentifier()).toBe("component = selector | attribute | auxiliary");
+    });
   });
 });
