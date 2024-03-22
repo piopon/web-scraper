2ccc1ae793febd325ddde8ba73bc5f3fc0f03ff5
Piotr Ponikowski <piopon.github@gmail.com>
2024-03-22 22:31:48 +0100

    initial version of scrap-component unit test file

---
+++ test/model/scrap-component.test.js
@@ -0,0 +1,8 @@
+import { ScrapComponent } from "../../src/model/scrap-component.js";
+
+describe("getIdentifier", () => {
+  test("returns correct result for empty component", () => {
+    const component = new ScrapComponent();
+    expect(component.getIdentifier()).toBe("component =  |  | ");
+  });
+});
