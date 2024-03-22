96f123d84ccefa1a939b150670ce496f996bf134
Piotr Ponikowski <piopon.github@gmail.com>
2024-03-22 22:41:26 +0100

    adjusted initial getIdentifier() unit test

--- test/model/scrap-component.test.js
+++ test/model/scrap-component.test.js
@@ -3,6 +3,6 @@ import { ScrapComponent } from "../../src/model/scrap-component.js";
 describe("getIdentifier", () => {
   test("returns correct result for empty component", () => {
     const component = new ScrapComponent();
-    expect(component.getIdentifier()).toBe("component =  |  | ");
+    expect(component.getIdentifier()).toBe("component = empty | empty | empty");
   });
 });
