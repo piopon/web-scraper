83316fbffb21dd53a9bbf7645d029a2fb6073965
Piotr Ponikowski <piopon.github@gmail.com>
2024-03-22 22:46:27 +0100

    reformatted scrap-component.test.js file

--- test/model/scrap-component.test.js
+++ test/model/scrap-component.test.js
@@ -7,15 +7,15 @@ describe("getIdentifier", () => {
       expect(component.getIdentifier()).toBe("component = empty | empty | empty");
     });
     test("with non-empty selector", () => {
-      const component = new ScrapComponent({selector: "selector"});
+      const component = new ScrapComponent({ selector: "selector" });
       expect(component.getIdentifier()).toBe("component = selector | empty | empty");
     });
     test("with non-empty attribute", () => {
-      const component = new ScrapComponent({ attribute: "attribute"});
+      const component = new ScrapComponent({ attribute: "attribute" });
       expect(component.getIdentifier()).toBe("component = empty | attribute | empty");
     });
     test("with non-empty auxiliary", () => {
-      const component = new ScrapComponent({ auxiliary: "auxiliary"});
+      const component = new ScrapComponent({ auxiliary: "auxiliary" });
       expect(component.getIdentifier()).toBe("component = empty | empty | auxiliary");
     });
   });
