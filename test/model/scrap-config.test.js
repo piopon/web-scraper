af9ec0bd52f0ed16a2671af0a96e25a4b2a74d85
Piotr Ponikowski <piopon.github@gmail.com>
2024-03-29 23:41:50 +0100

    added new scrap-config.test.js file with ScrapConfig.getIdentifier() tests

---
+++ test/model/scrap-config.test.js
@@ -0,0 +1,16 @@
+import { ScrapConfig } from "../../src/model/scrap-config.js";
+
+describe("getIdentifier", () => {
+  describe("returns correct result for group", () => {
+    test("with empty user value", () => {
+      const inputObj = {};
+      const expected = "user = empty";
+      expect(new ScrapConfig(inputObj).getIdentifier()).toBe(expected);
+    });
+    test("with non-empty user value", () => {
+      const inputObj = { user: "test-user" };
+      const expected = "user = test-user";
+      expect(new ScrapConfig(inputObj).getIdentifier()).toBe(expected);
+    });
+  });
+});
