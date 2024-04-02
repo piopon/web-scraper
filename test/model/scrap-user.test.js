69ea7326189411ca1397b628a9da4e46e46707f9
Piotr Ponikowski <piopon.github@gmail.com>
2024-04-03 00:35:39 +0200

    enhanced getDatabaseSchema test with specific values check

--- test/model/scrap-user.test.js
+++ test/model/scrap-user.test.js
@@ -1,5 +1,7 @@
 import { ScrapUser } from "../../src/model/scrap-user.js";
 
+import mongoose from "mongoose";
+
 describe("getDatabaseModel", () => {
   test("returns correct model names fields", () => {
     const TestModel = ScrapUser.getDatabaseModel();
@@ -17,5 +19,27 @@ describe("getDatabaseSchema", () => {
   test("returns correct value", () => {
     const schema = ScrapUser.getDatabaseSchema();
     expect(schema).not.toBe(null);
+  });
+  test("gets schema used for create config", () => {
+    const testDate = new Date();
+    const configId = new mongoose.Types.ObjectId();
+    const TestModel = mongoose.model("test-config", ScrapUser.getDatabaseSchema());
+    const config = new TestModel({
+      unknown: "test-unknown",
+      name: "test-name",
+      email: "test-email",
+      password: "test-password",
+      lastLogin: testDate,
+      config: configId,
+      extra: "test-extra",
+    });
+    expect(config).not.toBe(null);
+    expect(config.unknown).toBe(undefined);
+    expect(config.name).toBe("test-name");
+    expect(config.email).toBe("test-email");
+    expect(config.password).toBe("test-password");
+    expect(config.lastLogin).toBe(testDate);
+    expect(config.config).toBe(configId);
+    expect(config.extra).toBe(undefined);
   });
 });
