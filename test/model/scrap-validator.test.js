e43b8a32b980e2499820890985911e0909edf819
Piotr Ponikowski <piopon.github@gmail.com>
2024-04-03 20:06:44 +0200

    added test config, group, observer and component objects creation method

--- test/model/scrap-validator.test.js
+++ test/model/scrap-validator.test.js
@@ -1,4 +1,8 @@
 import { ScrapValidator } from "../../src/model/scrap-validator.js";
+import { ScrapConfig } from "../../src/model/scrap-config.js";
+import { ScrapGroup } from "../../src/model/scrap-group.js";
+import { ScrapObserver } from "../../src/model/scrap-observer.js";
+import { ScrapComponent } from "../../src/model/scrap-component.js";
 import { ScrapError, ScrapWarning } from "../../src/model/scrap-exception.js";
 
 describe("validate", () => {
@@ -14,3 +18,33 @@ describe("validate", () => {
     });
   });
 });
+
+function createTestConfig(variant) {
+  return new ScrapConfig({
+    user: "test-user",
+    groups: [
+      new ScrapGroup({
+        name: "test-group",
+        category: "WARN" === variant ? undefined : "test-category",
+        domain: "ERR" === variant ? undefined : "test-domain",
+        observers: [
+          new ScrapObserver({
+            name: "test-observer",
+            path: "test-path",
+            title: createTestComponent("title"),
+            image: createTestComponent("image"),
+            price: createTestComponent("price"),
+          }),
+        ],
+      }),
+    ],
+  });
+}
+
+function createTestComponent(type) {
+  return new ScrapComponent({
+    selector: `${type}-selector`,
+    attribute: `${type}-selector`,
+    auxiliary: `${type}-selector`,
+  });
+}
