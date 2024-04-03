51b9e6c5e1d566ed79db531e66fa6691575740ed
Piotr Ponikowski <piopon.github@gmail.com>
2024-04-03 19:47:25 +0200

    reorganized scrap validator tests with adding intermediate describe statement

--- test/model/scrap-validator.test.js
+++ test/model/scrap-validator.test.js
@@ -2,13 +2,15 @@ import { ScrapValidator } from "../../src/model/scrap-validator.js";
 import { ScrapError, ScrapWarning } from "../../src/model/scrap-exception.js";
 
 describe("validate", () => {
-  test("throws on empty config input parameter", () => {
-    expect(() => new ScrapValidator()).toThrow(ScrapError);
-  });
-  test("throws on undefined config input parameter", () => {
-    expect(() => new ScrapValidator(undefined)).toThrow(ScrapError);
-  });
-  test("throws on null config input parameter", () => {
-    expect(() => new ScrapValidator(null)).toThrow(ScrapError);
+  describe("throws when constructor", () => {
+    test("has empty parameter", () => {
+      expect(() => new ScrapValidator()).toThrow(ScrapError);
+    });
+    test("has undefined parameter", () => {
+      expect(() => new ScrapValidator(undefined)).toThrow(ScrapError);
+    });
+    test("has null parameter", () => {
+      expect(() => new ScrapValidator(null)).toThrow(ScrapError);
+    });
   });
 });
