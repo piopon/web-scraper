383ea85b03d8b3c82d0f37e0d1df50d82c649885
Piotr Ponikowski <piopon.github@gmail.com>
2024-04-05 18:28:08 +0200

    updated log level test description + added other static values

--- test/config/app-types.test.js
+++ test/config/app-types.test.js
@@ -2,9 +2,21 @@ import { LogLevel, ComponentType, ComponentStatus } from "../../config/app-types
 
 describe("LogLevel", () => {
   describe("has correct values for", () => {
-    test("level error", () => {
+    test("static type = ERROR", () => {
       expect(LogLevel.ERROR.value).toBe(0);
       expect(LogLevel.ERROR.description).toBe("ERROR");
+    });
+    test("static type = WARNING", () => {
+      expect(LogLevel.WARNING.value).toBe(1);
+      expect(LogLevel.WARNING.description).toBe("WARNING");
+    });
+    test("static type = INFO", () => {
+      expect(LogLevel.INFO.value).toBe(2);
+      expect(LogLevel.INFO.description).toBe("INFO");
+    });
+    test("static type = DEBUG", () => {
+      expect(LogLevel.DEBUG.value).toBe(3);
+      expect(LogLevel.DEBUG.description).toBe("DEBUG");
     });
   });
 });
