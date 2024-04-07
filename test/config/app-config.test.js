d4da861d64d6ab0c2a8936d3ef6d65dbbc34febf
Piotr Ponikowski <piopon.github@gmail.com>
2024-04-07 21:10:48 +0200

    added server config and database config checks

--- test/config/app-config.test.js
+++ test/config/app-config.test.js
@@ -7,7 +7,14 @@ describe("getConfig", () => {
     expect(testConfig.usersDataPath).toMatch(/web-scraper\\users/);
     expect(testConfig.minLogLevel).toBe(LogLevel.INFO);
     expect(testConfig.serverConfig).not.toBe(null);
+    expect(testConfig.serverConfig.port).toBe("5000");
     expect(testConfig.databaseConfig).not.toBe(null);
+    expect(testConfig.databaseConfig.url).toBe("localhost");
+    expect(testConfig.databaseConfig.name).toBe("web-scraper");
+    expect(testConfig.databaseConfig.port).toBe("27017");
+    expect(testConfig.databaseConfig.user).toBe("");
+    expect(testConfig.databaseConfig.password).toBe("");
+    expect(testConfig.databaseConfig.timeout).toBe(15_000);
     expect(testConfig.scraperConfig).not.toBe(null);
   });
 });
