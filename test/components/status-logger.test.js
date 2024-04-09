780cdb6f29b296408cdccddea1adec8facc57dea
Piotr Ponikowski <piopon.github@gmail.com>
2024-04-09 19:31:09 +0200

    adjusted currently existing StatusLogger unit test descriptions

--- test/components/status-logger.test.js
+++ test/components/status-logger.test.js
@@ -1,19 +1,19 @@
 import { StatusLogger } from "../../src/components/status-logger.js";
 import { LogLevel } from "../../config/app-types.js";
 
-test("getName returns correct result", () => {
+test("getName() returns correct result", () => {
   const testLogger = new StatusLogger("test-name", LogLevel.INFO);
   expect(testLogger.getName()).toBe("test-name");
 });
 
-describe("debug log", () => {
-  test("will appear when min log is equal/lower than LogLevel.DEBUG", () => {
+describe("debug() log", () => {
+  test("will appear when setting is equal/lower than LogLevel.DEBUG", () => {
     const testLogger = new StatusLogger("test-name", LogLevel.DEBUG);
     testLogger.debug("test debug log");
     expect(testLogger.getStatus().type).toBe("debug");
     expect(testLogger.getStatus().message).toBe("test debug log");
   });
-  test("will NOT appear when min log is higher than LogLevel.DEBUG", () => {
+  test("will NOT appear when setting is higher than LogLevel.DEBUG", () => {
     const testLogger = new StatusLogger("test-name", LogLevel.INFO);
     testLogger.debug("test debug log");
     expect(testLogger.getStatus().type).toBe("");
