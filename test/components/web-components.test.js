452a21c46cf1b7b359e53dbc010ee4271e3d4d8a
Piotr Ponikowski <piopon.github@gmail.com>
2024-04-11 23:04:02 +0200

    added new WebComponents test verifying getStatus() logic

--- test/components/web-components.test.js
+++ test/components/web-components.test.js
@@ -1,8 +1,14 @@
 import { WebComponents } from "../../src/components/web-components.js";
-import { LogLevel } from "../../config/app-types.js";
+import { LogLevel, ComponentStatus } from "../../config/app-types.js";
 
 test("getName() returns correct result", () => {
   const inConfig = { minLogLevel: LogLevel.INFO };
   const testComponent = new WebComponents(inConfig);
   expect(testComponent.getName()).toBe("web-components");
+});
+
+test("getStatus() returns correct result", () => {
+  const inConfig = { minLogLevel: LogLevel.INFO };
+  const testComponent = new WebComponents(inConfig);
+  expect(testComponent.getStatus()).toBe(ComponentStatus.RUNNING);
 });
