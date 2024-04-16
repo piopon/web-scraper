a82d8d6b3aa661857bf896dbe2199f3dfce926ae
Piotr Ponikowski <piopon.github@gmail.com>
2024-04-16 23:41:00 +0200

    updated createTestComponent() method to contain component and master names

--- test/components/web-components.test.js
+++ test/components/web-components.test.js
@@ -17,13 +17,13 @@ describe("addComponent()", () => {
   test("correctly adds valid components", () => {
     const inConfig = { minLogLevel: LogLevel.INFO };
     const testComponent = new WebComponents(inConfig);
-    testComponent.addComponent(createTestComponent([ComponentType.CONFIG]));
+    testComponent.addComponent(createTestComponent("foo", [ComponentType.CONFIG]));
     expect(testComponent.getComponents().length).toBe(1);
   });
   test("adds component without type but prints warning", () => {
     const inConfig = { minLogLevel: LogLevel.INFO };
     const testComponent = new WebComponents(inConfig);
-    testComponent.addComponent(createTestComponent([]));
+    testComponent.addComponent(createTestComponent("foo", []));
     expect(testComponent.getComponents().length).toBe(1);
     const statusHistory = testComponent.getHistory();
     expect(statusHistory.length).toBe(2);
@@ -35,26 +35,26 @@ describe("addComponent()", () => {
   test("does add slave-only component when master IS defined", () => {
     const inConfig = { minLogLevel: LogLevel.INFO };
     const testComponent = new WebComponents(inConfig);
-    testComponent.addComponent(createTestComponent([ComponentType.CONFIG]));
-    testComponent.addComponent(createTestComponent([ComponentType.SLAVE]));
+    testComponent.addComponent(createTestComponent("mas1", [ComponentType.CONFIG]));
+    testComponent.addComponent(createTestComponent("foo", [ComponentType.SLAVE], "mas1"));
     expect(testComponent.getComponents().length).toBe(1);
   });
   test("does not add slave-only component when master NOT defined", () => {
     const inConfig = { minLogLevel: LogLevel.INFO };
     const testComponent = new WebComponents(inConfig);
-    testComponent.addComponent(createTestComponent([ComponentType.SLAVE]));
+    testComponent.addComponent(createTestComponent("foo", [ComponentType.SLAVE]));
     expect(testComponent.getComponents().length).toBe(0);
   });
   test("does add slave component when master not defined but other type is used", () => {
     const inConfig = { minLogLevel: LogLevel.INFO };
     const testComponent = new WebComponents(inConfig);
-    testComponent.addComponent(createTestComponent([ComponentType.SLAVE, ComponentType.CONFIG]));
+    testComponent.addComponent(createTestComponent("foo", [ComponentType.SLAVE, ComponentType.CONFIG]));
     expect(testComponent.getComponents().length).toBe(1);
   });
   test("throws when input object is incorrect", () => {
     const inConfig = { minLogLevel: LogLevel.INFO };
     const testComponent = new WebComponents(inConfig);
-    expect(() => testComponent.addComponent(createTestComponent([ComponentType.AUTH]))).toThrow(Error);
+    expect(() => testComponent.addComponent(createTestComponent("foo", [ComponentType.AUTH]))).toThrow(Error);
     expect(testComponent.getComponents().length).toBe(0);
     const statusHistory = testComponent.getHistory();
     expect(statusHistory.length).toBe(2);
@@ -65,21 +65,21 @@ describe("addComponent()", () => {
   });
 });
 
-function createTestComponent(types) {
+function createTestComponent(componentName, componentTypes, masterName = "") {
   return {
     getName() {
-      return "foo";
+      return componentName;
     },
     getInfo() {
       return {
-        types: types,
+        types: componentTypes,
         initWait: false,
       };
     },
     update() {},
     getMaster() {
       return {
-        name: "foo",
+        name: masterName,
       };
     },
   };
