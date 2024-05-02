7f17d63aaed94337f3302e57a7e53168fe9ac94c
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-02 23:05:52 +0200

    fixed web-database test suite to check constructor

--- test/components/web-database.test.js
+++ test/components/web-database.test.js
@@ -1,12 +1,13 @@
 import { WebDatabase } from "../../src/components/web-database.js";
 import { LogLevel } from "../../config/app-types.js";
 
-describe("getName()", () => {
-  test("returns correct result when log level is defined", () => {
-    const testDatabase = new WebDatabase({ minLogLevel: LogLevel.INFO });
-    expect(testDatabase.getName()).toBe("web-database  ");
+describe("creating an object", () => {
+  test("instantiates a new object when input object is correct", () => {
+    const inputObject = { minLogLevel: LogLevel.INFO };
+    expect(() => new WebDatabase(inputObject)).not.toThrow(Error);
   });
   test("throws when no log level is defined", () => {
-    expect(() => new WebDatabase()).toThrow(TypeError);
+    const inputObject = {};
+    expect(() => new WebDatabase(inputObject)).toThrow(TypeError);
   });
 });
