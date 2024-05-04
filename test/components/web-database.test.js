85480f40d2bb5bc8f55edfd2fa3e16672117864a
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-04 20:18:23 +0200

    added web-database component start() test with mock impl verification

--- test/components/web-database.test.js
+++ test/components/web-database.test.js
@@ -1,6 +1,9 @@
 import { WebDatabase } from "../../src/components/web-database.js";
 import { ComponentType, LogLevel } from "../../config/app-types.js";
 
+import { jest } from "@jest/globals";
+import mongoose from "mongoose";
+
 describe("creating an object", () => {
   test("instantiates a new object when input object is correct", () => {
     const inputObject = { minLogLevel: LogLevel.INFO };
@@ -31,4 +34,27 @@ test("getInfo() returns correct result", () => {
   expect(infoObject).not.toBe(undefined);
   expect(infoObject.types).toStrictEqual([ComponentType.INIT]);
   expect(infoObject.initWait).toBe(false);
+});
+
+test("start() has correct input data", async () => {
+  const configObject = {
+    name: "test-name",
+    user: "user-name",
+    password: "pass",
+    timeout: 1_000,
+  };
+  const testDatabase = new WebDatabase({ minLogLevel: LogLevel.INFO, databaseConfig: configObject });
+  const mongooseConnectSpyOn = jest.spyOn(mongoose, "connect").mockImplementationOnce(() => Promise.resolve(mongoose));
+  await testDatabase.start();
+  const expectedUrl = "mongodb://undefined:undefined";
+  const expectedObj = {
+    appName: configObject.name,
+    dbName: configObject.name,
+    user: configObject.user,
+    pass: configObject.password,
+    serverSelectionTimeoutMS: configObject.timeout,
+    connectTimeoutMS: configObject.timeout,
+    family: 4,
+  };
+  expect(mongooseConnectSpyOn).toBeCalledWith(expectedUrl, expectedObj);
 });
