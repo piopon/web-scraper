80bc22edc29ec3a8fad223cb0cd1b16313980545
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-13 23:09:49 +0200

    added new test for retrieving getInfo for WebScraper

--- test/components/web-scraper.test.js
+++ test/components/web-scraper.test.js
@@ -5,3 +5,11 @@ test("getName() returns correct result", () => {
   const testScraper = new WebScraper({ minLogLevel: LogLevel.INFO });
   expect(testScraper.getName()).toBe("web-scraper   ");
 });
+
+test("getInfo() returns correct result", () => {
+  const testScraper = new WebScraper({ minLogLevel: LogLevel.INFO });
+  const infoObject = testScraper.getInfo();
+  expect(infoObject).not.toBe(undefined);
+  expect(infoObject.types).toStrictEqual([ComponentType.SLAVE, ComponentType.CONFIG, ComponentType.AUTH]);
+  expect(infoObject.initWait).toBe(false);
+});
