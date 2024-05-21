995a416564c9d1037053a583ba00204a36d84f30
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-21 20:36:39 +0200

    bugfix: corrected failure test for start() method to return null mock result

--- test/components/web-scraper.test.js
+++ test/components/web-scraper.test.js
@@ -59,7 +59,7 @@ describe("start() method", () => {
   });
   test("fails when specified user configuration is missing", async () => {
     const userConfig = { user: "ID", groups: [] };
-    const mockResult = { findById: () => ({ toJSON: () => userConfig }) };
+    const mockResult = null;
     jest.spyOn(ScrapConfig, "getDatabaseModel").mockImplementationOnce(() => mockResult);
     const result = await testScraper.start({ name: "test", email: "mail", config: userConfig });
     expect(result).toBe(false);
