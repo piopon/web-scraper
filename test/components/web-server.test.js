d28812f01e8e2b0b190eb981f9d7e2d1df378511
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-25 23:24:57 +0200

    checking different express listen() mock approach

--- test/components/web-server.test.js
+++ test/components/web-server.test.js
@@ -2,17 +2,20 @@ import { WebServer } from "../../src/components/web-server.js";
 import { WebComponents } from "../../src/components/web-components.js";
 import { LogLevel } from "../../config/app-types.js";
 
+import { jest } from "@jest/globals";
 import express from "express";
 
 test("run() should start the server by calling listen() exactly once", async () => {
   const config = { minLogLevel: LogLevel.INFO, serverConfig: { port: 123 } };
   const testServer = new WebServer(config, new WebComponents(config));
+  const listenMock = jest.fn();
+  jest.mock('express', () => {
+    return jest.fn(() => ({
+      listen: listenMock
+    }));
+  });
   await testServer.run();
 
-  // Get the mocked express instance
-  const mockedExpressInstance = express();
-  // Check if the listen method was called once
-  expect(mockedExpressInstance.listen).toHaveBeenCalledTimes(1);
-  // Optionally, check if listen was called with the correct port
-  expect(mockedExpressInstance.listen).toHaveBeenCalledWith(3000, expect.any(Function));
+  expect(listenMock).toHaveBeenCalledTimes(1);
+  expect(listenMock).toHaveBeenCalledWith(123, expect.any(Function));
 });
