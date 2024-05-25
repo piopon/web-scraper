85fdc0d99ca0c164228dea2df9c2bcf4e6709223
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-25 23:27:00 +0200

    added clearAllMocks and adjusted listen mock + explicitly calling express ctor

--- test/components/web-server.test.js
+++ test/components/web-server.test.js
@@ -6,16 +6,16 @@ import { jest } from "@jest/globals";
 import express from "express";
 
 test("run() should start the server by calling listen() exactly once", async () => {
+  jest.clearAllMocks();
   const config = { minLogLevel: LogLevel.INFO, serverConfig: { port: 123 } };
   const testServer = new WebServer(config, new WebComponents(config));
-  const listenMock = jest.fn();
   jest.mock('express', () => {
+    const listen = jest.fn();
     return jest.fn(() => ({
-      listen: listenMock
+      listen
     }));
   });
   await testServer.run();
-
-  expect(listenMock).toHaveBeenCalledTimes(1);
-  expect(listenMock).toHaveBeenCalledWith(123, expect.any(Function));
+  expect(express().listen).toHaveBeenCalledTimes(1);
+  expect(express().listen).toHaveBeenCalledWith(123, expect.any(Function));
 });
