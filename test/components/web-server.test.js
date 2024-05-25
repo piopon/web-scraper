668bd953ad8cdea2143d990688317e5d3d338a86
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-25 23:28:56 +0200

    moved mock outside test case + added express ctro call number check

--- test/components/web-server.test.js
+++ test/components/web-server.test.js
@@ -5,17 +5,20 @@ import { LogLevel } from "../../config/app-types.js";
 import { jest } from "@jest/globals";
 import express from "express";
 
+const listenMock = jest.fn();
+const expressMock = jest.fn(() => ({
+  listen: listenMock
+}));
+
+jest.mock('express', () => expressMock);
+
 test("run() should start the server by calling listen() exactly once", async () => {
   jest.clearAllMocks();
   const config = { minLogLevel: LogLevel.INFO, serverConfig: { port: 123 } };
   const testServer = new WebServer(config, new WebComponents(config));
-  jest.mock('express', () => {
-    const listen = jest.fn();
-    return jest.fn(() => ({
-      listen
-    }));
-  });
+
   await testServer.run();
-  expect(express().listen).toHaveBeenCalledTimes(1);
-  expect(express().listen).toHaveBeenCalledWith(123, expect.any(Function));
+  expect(expressMock).toHaveBeenCalledTimes(1);
+  expect(listenMock).toHaveBeenCalledTimes(1);
+  expect(listenMock).toHaveBeenCalledWith(123, expect.any(Function));
 });
