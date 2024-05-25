6f0edc7f8c9c0076e154038650f8f47cecb49762
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-26 00:07:58 +0200

    prepared web-server test for checking different start cases

--- test/components/web-server.test.js
+++ test/components/web-server.test.js
@@ -2,14 +2,16 @@ import { WebServer } from "../../src/components/web-server.js";
 import { WebComponents } from "../../src/components/web-components.js";
 import { LogLevel } from "../../config/app-types.js";
 
-test("run() should start the server and not throw", async () => {
-  const config = { minLogLevel: LogLevel.INFO, serverConfig: { port: 123 } };
-  const testServer = new WebServer(config, new WebComponents(config));
-  try {
-    const result = await testServer.run();
-    expect(result).toBe(true);
-    testServer.shutdown();
-  } catch (error) {
-    fail("Run should NOT throw");
-  }
+describe("run() method", () => {
+  test("should start server and not throw when no INIT components", async () => {
+    const config = { minLogLevel: LogLevel.INFO, serverConfig: { port: 123 } };
+    const testServer = new WebServer(config, new WebComponents(config));
+    try {
+      const result = await testServer.run();
+      expect(result).toBe(true);
+      testServer.shutdown();
+    } catch (error) {
+      fail("Run should NOT throw");
+    }
+  });
 });
