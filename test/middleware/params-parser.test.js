a8764d2f5b834d7b55ee6d6f9ff25d521e7e4d6e
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-28 19:33:59 +0200

    added new test checking unsigned int in request query params

--- test/middleware/params-parser.test.js
+++ test/middleware/params-parser.test.js
@@ -12,4 +12,13 @@ describe("middleware() method", () => {
     expect(requestObj.params.p2).toBe("test");
     expect(requestObj.query.p3).toBe(-123);
   });
+  test("should correctly parse unsigned integer in query", async () => {
+    const requestObj = { params: { p1: "-123", p2: "test" }, query: { p3: "123" } };
+    const mockedRes = jest.fn();
+    const mockedNext = jest.fn();
+    ParamsParser.middleware(requestObj, mockedRes, mockedNext);
+    expect(requestObj.params.p1).toBe(-123);
+    expect(requestObj.params.p2).toBe("test");
+    expect(requestObj.query.p3).toBe(123);
+  });
 });
