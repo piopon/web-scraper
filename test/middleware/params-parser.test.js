b566460e4caf22c1763c5093cfc2f6ac1a2c7536
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-28 19:31:36 +0200

    adjusted params parser middleware test for unsigned integer with more params

--- test/middleware/params-parser.test.js
+++ test/middleware/params-parser.test.js
@@ -1,13 +1,15 @@
-import { ParamsParser } from "../../src/middleware/params-parser.js"
+import { ParamsParser } from "../../src/middleware/params-parser.js";
 
 import { jest } from "@jest/globals";
 
 describe("middleware() method", () => {
   test("should correctly parse unsigned integer in params", async () => {
-    const requestObj = { params: {param1: "123"} };
+    const requestObj = { params: { p1: "123", p2: "test" }, query: { p3: "-123" } };
     const mockedRes = jest.fn();
     const mockedNext = jest.fn();
     ParamsParser.middleware(requestObj, mockedRes, mockedNext);
-    expect(requestObj.params.param1).toBe(123);
+    expect(requestObj.params.p1).toBe(123);
+    expect(requestObj.params.p2).toBe("test");
+    expect(requestObj.query.p3).toBe(-123);
   });
 });
