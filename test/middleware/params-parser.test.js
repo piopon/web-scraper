6bb3b7b345c0d4024291f8c1b95c915b73983f6b
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-28 19:47:23 +0200

    added ALL typical parameters parsing test

--- test/middleware/params-parser.test.js
+++ test/middleware/params-parser.test.js
@@ -39,4 +39,19 @@ describe("middleware() method", () => {
     expect(requestObj.params.p2).toBe("tru");
     expect(requestObj.query.p3).toBe(false);
   });
+  test("should correctly parse all different params", async () => {
+    const requestObj = {
+      params: { p1: "1.23", p2: "-4.56", p3: "-123" },
+      query: { p4: "456", p5: "false", p6: "null" },
+    };
+    const mockedRes = jest.fn();
+    const mockedNext = jest.fn();
+    ParamsParser.middleware(requestObj, mockedRes, mockedNext);
+    expect(requestObj.params.p1).toBe("1.23");
+    expect(requestObj.params.p2).toBe("-4.56");
+    expect(requestObj.params.p3).toBe(-123);
+    expect(requestObj.query.p4).toBe(456);
+    expect(requestObj.query.p5).toBe(false);
+    expect(requestObj.query.p6).toBe("null");
+  });
 });
