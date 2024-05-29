4a6eac6aef7e8d20cb18c65b8c0df3986e90bae3
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-29 22:45:48 +0200

    added new test checking that user will be redirected when not authorized

--- test/middleware/access-checker.test.js
+++ test/middleware/access-checker.test.js
@@ -10,4 +10,13 @@ describe("canViewContent() method", () => {
     expect(invokeState.redirect).toBe("");
     expect(invokeState.next).toBe(true);
   });
+  test("should redirect when not authorized", async () => {
+    const requestObj = { isAuthenticated: () => false };
+    const invokeState = { redirect: "", next: false };
+    const mockedRes = { redirect: (input) => (invokeState.redirect = input) };
+    const mockedNext = () => (invokeState.next = true);
+    AccessChecker.canViewContent(requestObj, mockedRes, mockedNext);
+    expect(invokeState.redirect).toBe("/auth/login");
+    expect(invokeState.next).toBe(false);
+  });
 });
