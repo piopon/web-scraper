e1a3e0d607cc4bb473805ad7350c38d1145f2ae4
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-29 22:46:13 +0200

    checking if canViewSessionUser() does NOT redirect when not auth

--- test/middleware/access-checker.test.js
+++ test/middleware/access-checker.test.js
@@ -20,3 +20,15 @@ describe("canViewContent() method", () => {
     expect(invokeState.next).toBe(false);
   });
 });
+
+describe("canViewSessionUser() method", () => {
+  test("should not redirect when not authorized", async () => {
+    const requestObj = { isAuthenticated: () => false };
+    const invokeState = { redirect: "", next: false };
+    const mockedRes = { redirect: (input) => (invokeState.redirect = input) };
+    const mockedNext = () => (invokeState.next = true);
+    AccessChecker.canViewSessionUser(requestObj, mockedRes, mockedNext);
+    expect(invokeState.redirect).toBe("");
+    expect(invokeState.next).toBe(true);
+  });
+});
