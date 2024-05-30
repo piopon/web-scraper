f65328ff05a4189def8685205e78ca622a51601a
Piotr Ponikowski <piopon.github@gmail.com>
2024-05-30 11:56:04 +0200

    bugfix: added missing jest framework import statement

--- test/middleware/request-logger.test.js
+++ test/middleware/request-logger.test.js
@@ -2,6 +2,8 @@ import { RequestLogger } from "../../src/middleware/request-logger.js";
 import { StatusLogger } from "../../src/components/status-logger.js";
 import { LogLevel } from "../../config/app-types.js";
 
+import { jest } from "@jest/globals";
+
 test("middleware() should correctly log request using logger", async () => {
   const logger = new StatusLogger("test", LogLevel.DEBUG);
   const requestObj = { method: "TEMP", protocol: "proto", originalUrl: "url", get: (input) => "host/" };
