484fd2c67d81aa566a5186c24d0847c18e736775
Piotr Ponikowski <piopon.github@gmail.com>
2024-06-11 18:24:25 +0200

    added new unit test checking incompatible body and query id when update

--- test/routes/api/config-router.test.js
+++ test/routes/api/config-router.test.js
@@ -267,6 +267,25 @@ describe("created config PUT routes", () => {
     expect(response.statusCode).toBe(200);
     expect(response.body).toStrictEqual("Edited configuration element with name = test1");
   });
+  test("returns bad request error for path '/groups' when query and body are incompatible", async () => {
+    const testObj = {
+      name: "test1",
+      category: "%%%",
+      domain: "new.com",
+      observers: [
+        {
+          name: "logo",
+          path: "info",
+          target: "load",
+          history: "off",
+          price: { interval: "1D", selector: "title", attribute: "innerText", auxiliary: "CAD" },
+        },
+      ],
+    };
+    const response = await testAgent.put("/config/groups").query({ name: "test2" }).send(testObj);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toStrictEqual("Incompatible query (name = test2) and body (name = test1) identifiers");
+  });
 });
 
 function createMockAuthRouter() {
