77b54813b26f92680b1c69a6694412ee2c7c55e0
Piotr Ponikowski <piopon.github@gmail.com>
2024-06-17 19:40:01 +0200

    added new test suite for checking unknown path while POSTing new data

--- test/routes/api/config-router.test.js
+++ test/routes/api/config-router.test.js
@@ -301,6 +301,31 @@ describe("created config PUT routes", () => {
   });
 });
 
+describe("created config POST routes", () => {
+  const components = new WebComponents({ minLogLevel: LogLevel.DEBUG });
+  const mockResult = { findById: (configId) => getInitConfig(false, configId, "uname") };
+  jest.spyOn(ScrapConfig, "getDatabaseModel").mockImplementation(() => mockResult);
+  // configue test express app server
+  const testApp = express();
+  testApp.use(express.json());
+  testApp.use(express.urlencoded({ extended: false }));
+  testApp.use(session({ secret: "unit_tests", resave: false, saveUninitialized: false }));
+  testApp.use(passport.initialize());
+  testApp.use(passport.session());
+  testApp.use("/config", new ConfigRouter(components).createRoutes());
+  testApp.use("/auth", createMockAuthRouter());
+  // retrieve underlying superagent to correctly persist sessions
+  const testAgent = supertest.agent(testApp);
+  beforeAll(async () => {
+    const mockAuth = { mail: "test@mail.com", pass: "test-secret" };
+    await testAgent.post("/auth/login").send(mockAuth);
+  });
+  test("returns correct result for unknown path", async () => {
+    const response = await testAgent.post("/configs/unknown");
+    expect(response.statusCode).toBe(404);
+  });
+});
+
 function createMockAuthRouter() {
   const router = express.Router();
   const configId = 123;
