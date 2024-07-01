0d463191eca0b38fc397815d081351624c6678ea
Piotr Ponikowski <piopon.github@gmail.com>
2024-07-01 22:29:11 +0200

    added missing imports to status router test

--- test/routes/api/status-router.test.js
+++ test/routes/api/status-router.test.js
@@ -2,6 +2,12 @@ import { StatusRouter } from "../../../src/routes/api/status-router.js";
 import { WebComponents } from "../../../src/components/web-components.js";
 import { LogLevel } from "../../../config/app-types.js";
 
+import supertest from "supertest";
+import passport from "passport";
+import express from "express";
+import session from "express-session";
+import { jest } from "@jest/globals";
+
 describe("createRoutes() method", () => {
   test("returns correct number of routes", () => {
     const expectedRoutes = [{ path: "/", method: "get" }];
