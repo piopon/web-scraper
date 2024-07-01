d45b16c19fb35997c9082357f6be83f06eeb2aa5
Piotr Ponikowski <piopon.github@gmail.com>
2024-07-01 22:28:12 +0200

    added new initial test suite for checking GET routes

--- test/routes/api/status-router.test.js
+++ test/routes/api/status-router.test.js
@@ -23,3 +23,30 @@ describe("createRoutes() method", () => {
       });
   });
 });
+
+describe("created config GET routes", () => {
+  const serverHistory = {};
+  const serverStatus = { getName: () => "Running", getHistory: () => serverHistory };
+  const components = new WebComponents({ minLogLevel: LogLevel.DEBUG });
+  const mockResult = { findById: (configId) => getInitConfig(false, configId, "uname") };
+  jest.spyOn(ScrapConfig, "getDatabaseModel").mockImplementation(() => mockResult);
+  // configue test express app server
+  const testApp = express();
+  testApp.use(express.json());
+  testApp.use(express.urlencoded({ extended: false }));
+  testApp.use(session({ secret: "unit_tests", resave: false, saveUninitialized: false }));
+  testApp.use(passport.initialize());
+  testApp.use(passport.session());
+  testApp.use("/status", new StatusRouter(serverStatus, components).createRoutes());
+  testApp.use("/auth", createMockAuthRouter());
+  // retrieve underlying superagent to correctly persist sessions
+  const testAgent = supertest.agent(testApp);
+  beforeAll(async () => {
+    const mockAuth = { mail: "test@mail.com", pass: "test-secret" };
+    await testAgent.post("/auth/login").send(mockAuth);
+  });
+  test("returns correct result for unknown path", async () => {
+    const response = await testAgent.get("/status/unknown");
+    expect(response.statusCode).toBe(404);
+  });
+});
