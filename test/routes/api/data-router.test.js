2f81b636be4f8d6ac17764c8dfac307bbf7dfa63
Piotr Ponikowski <piopon.github@gmail.com>
2024-07-08 00:23:30 +0200

    added final data router parameterized test checking not existing category query

--- test/routes/api/data-router.test.js
+++ test/routes/api/data-router.test.js
@@ -151,6 +151,14 @@ describe("created config GET routes", () => {
           ],
         },
       ],
+      [
+        "query contains mot existing name",
+        { category: "unknown" },
+        {
+          status: 200,
+          response: [],
+        },
+      ],
     ])("%s", async (_, inputQuery, expected) => {
       const response = await testClient.get("/data").query(inputQuery);
       expect(response.statusCode).toBe(expected.status);
