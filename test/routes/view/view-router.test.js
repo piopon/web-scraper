d251a25916e3a150b6eccc08eb0a383af914ac97
Piotr Ponikowski <piopon.github@gmail.com>
2024-07-10 22:54:40 +0200

    reformatted view-router.test.js file

--- test/routes/view/view-router.test.js
+++ test/routes/view/view-router.test.js
@@ -5,7 +5,10 @@ import express from "express";
 
 describe("createRoutes() method", () => {
   test("returns correct number of routes", () => {
-    const expectedRoutes = [{ path: "/", method: "get" }, { path: "/status", method: "get" }];
+    const expectedRoutes = [
+      { path: "/", method: "get" },
+      { path: "/status", method: "get" },
+    ];
     const testRouter = new ViewRouter();
     const createdRoutes = testRouter.createRoutes();
     expect(createdRoutes.stack.length).toBe(expectedRoutes.length);
