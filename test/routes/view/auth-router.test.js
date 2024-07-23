6150578e858200dab791b4f990a2b72a1bc33817
Piotr Ponikowski <piopon.github@gmail.com>
2024-07-23 20:18:36 +0200

    enhanced auth routes test with checking both URL and method

--- test/routes/view/auth-router.test.js
+++ test/routes/view/auth-router.test.js
@@ -17,5 +17,15 @@ describe("createRoutes() method", () => {
       const testRouter = new AuthRouter(components, passport);
       const createdRoutes = testRouter.createRoutes();
       expect(createdRoutes.stack.length).toBe(expectedRoutes.length);
+      createdRoutes.stack
+        .map((stackRoute) => stackRoute.route)
+        .forEach((route) => {
+          const foundRoutesNo = expectedRoutes.filter((expected) => {
+            const samePath = expected.path === route.path;
+            const hasMethod = Object.keys(route.methods).includes(expected.method);
+            return samePath && hasMethod;
+          }).length;
+          expect(foundRoutesNo).toBe(1);
+        });
     });
   });
