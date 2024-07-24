769d4425e42fb52bdc6fc48c8ce7b263af4318c9
Piotr Ponikowski <piopon.github@gmail.com>
2024-07-24 18:17:31 +0200

    reformatted auth-router.test.js file

--- test/routes/view/auth-router.test.js
+++ test/routes/view/auth-router.test.js
@@ -5,27 +5,27 @@ import { LogLevel } from "../../../config/app-types.js";
 import passport from "passport";
 
 describe("createRoutes() method", () => {
-    test("returns correct number of routes", () => {
-      const expectedRoutes = [
-        { path: "/register", method: "get" },
-        { path: "/login", method: "get" },
-        { path: "/register", method: "post" },
-        { path: "/login", method: "post" },
-        { path: "/logout", method: "post" },
-      ];
-      const components = new WebComponents({ minLogLevel: LogLevel.DEBUG });
-      const testRouter = new AuthRouter(components, passport);
-      const createdRoutes = testRouter.createRoutes();
-      expect(createdRoutes.stack.length).toBe(expectedRoutes.length);
-      createdRoutes.stack
-        .map((stackRoute) => stackRoute.route)
-        .forEach((route) => {
-          const foundRoutesNo = expectedRoutes.filter((expected) => {
-            const samePath = expected.path === route.path;
-            const hasMethod = Object.keys(route.methods).includes(expected.method);
-            return samePath && hasMethod;
-          }).length;
-          expect(foundRoutesNo).toBe(1);
-        });
-    });
+  test("returns correct number of routes", () => {
+    const expectedRoutes = [
+      { path: "/register", method: "get" },
+      { path: "/login", method: "get" },
+      { path: "/register", method: "post" },
+      { path: "/login", method: "post" },
+      { path: "/logout", method: "post" },
+    ];
+    const components = new WebComponents({ minLogLevel: LogLevel.DEBUG });
+    const testRouter = new AuthRouter(components, passport);
+    const createdRoutes = testRouter.createRoutes();
+    expect(createdRoutes.stack.length).toBe(expectedRoutes.length);
+    createdRoutes.stack
+      .map((stackRoute) => stackRoute.route)
+      .forEach((route) => {
+        const foundRoutesNo = expectedRoutes.filter((expected) => {
+          const samePath = expected.path === route.path;
+          const hasMethod = Object.keys(route.methods).includes(expected.method);
+          return samePath && hasMethod;
+        }).length;
+        expect(foundRoutesNo).toBe(1);
+      });
   });
+});
