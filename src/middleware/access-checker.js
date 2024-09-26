2b6279b4f4fe1fba530cc7af52380234b53af8b4
Piotr Ponikowski <piopon.github@gmail.com>
2024-09-26 21:07:38 +0200

    added method description to receive data access checker

--- src/middleware/access-checker.js
+++ src/middleware/access-checker.js
@@ -1,4 +1,10 @@
 export class AccessChecker {
+  /**
+   * Method used to check if the current request is eligible to access content data
+   * @param {Object} request The incoming request object
+   * @param {Object} response The outputted response object
+   * @param {Function} next The next middleware function in the cycle
+   */
   static canReceiveData(request, response, next) {
     if (request.isAuthenticated()) {
       return next();
