94f33430ec24a81018435fa48d09830be7d62967
Piotr Ponikowski <piopon.github@gmail.com>
2024-09-27 22:59:09 +0200

    testing unauthorized error case for receive data access checker

--- test/middleware/access-checker.test.js
+++ test/middleware/access-checker.test.js
@@ -16,6 +16,21 @@ describe("canReceiveData() method", () => {
     expect(invokeState.text).toBe("");
     expect(invokeState.next).toBe(true);
   });
+  test("should throw error 401 when not authorized", async () => {
+    const requestObj = { isAuthenticated: () => false };
+    const invokeState = { status: 200, text: "", next: false };
+    const mockedRes = {
+      status: (input) => {
+        invokeState.status = input;
+        return { json: (input) => (invokeState.text = input) };
+      },
+    };
+    const mockedNext = () => (invokeState.next = true);
+    AccessChecker.canReceiveData(requestObj, mockedRes, mockedNext);
+    expect(invokeState.status).toBe(401);
+    expect(invokeState.text).toBe("Not authenticated");
+    expect(invokeState.next).toBe(false);
+  });
 });
 
 describe("canViewContent() method", () => {
