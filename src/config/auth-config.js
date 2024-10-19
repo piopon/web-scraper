ca2a929bc90cb01a82a7cb62cf63e5a8788b5647
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-19 20:03:38 +0200

    added logic for serializing and deserializing users in configure methods

--- src/config/auth-config.js
+++ src/config/auth-config.js
@@ -1,3 +1,5 @@
+import { ScrapUser } from "../model/scrap-user.js";
+
 export class AuthConfig {
   #passport = undefined;
 
@@ -6,6 +8,14 @@ export class AuthConfig {
   }
 
   configure() {
+    // configure common serialize and deserialize user logic
+    this.#passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+    this.#passport.deserializeUser(async (id, done) => {
+      const user = await ScrapUser.getDatabaseModel().findById(id);
+      done(null, user);
+    });
     return this.#passport;
   }
 }
