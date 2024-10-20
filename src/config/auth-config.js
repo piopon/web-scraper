a2fbd92e5e9ba2e03702151110dbe3888345471b
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-20 20:22:15 +0200

    added JWT strategy configuration to AuthConfig class

--- src/config/auth-config.js
+++ src/config/auth-config.js
@@ -1,5 +1,7 @@
 import { ScrapUser } from "../model/scrap-user.js";
 
+import { ExtractJwt, Strategy } from "passport-jwt";
+
 export class AuthConfig {
   #passport = undefined;
 
@@ -7,7 +9,12 @@ export class AuthConfig {
     this.#passport = passport;
   }
 
+  /**
+   * Method used to configure Passport authentication object
+   */
   configure() {
+    // configure authenticate logic for specific endpoints
+    this.#configJwtStategy();
     // configure common serialize and deserialize user logic
     this.#passport.serializeUser((user, done) => {
       done(null, user._id);
@@ -17,5 +24,28 @@ export class AuthConfig {
       done(null, user);
     });
     return this.#passport;
+  }
+
+  /**
+   * Method used to configurate user JWT login strategy
+   * @param {Object} passport The login auth and stategy object
+   */
+  #configJwtStategy() {
+    const options = {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    };
+    const verify = async (jwtPayload, done) => {
+      try {
+        const user = await ScrapUser.getDatabaseModel().findById(jwtPayload._id);
+        if (user) {
+          return done(null, user);
+        }
+        return done(null, false, { message: "Incorrect token." });
+      } catch (error) {
+        return done(null, false, { message: error.message });
+      }
+    };
+    this.#passport.use("jwt", new Strategy(options, verify));
   }
 }
