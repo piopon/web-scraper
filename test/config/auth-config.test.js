421dce1b4cebf46afada816a82cc50b2ee081f61
Piotr Ponikowski <piopon.github@gmail.com>
2024-10-21 19:26:16 +0200

    added initial version of auth-config.test.js file

---
+++ test/config/auth-config.test.js
@@ -0,0 +1,12 @@
+import { WebComponents } from "../../src/components/web-components";
+import { LogLevel } from "../../src/config/app-types";
+import { AuthConfig } from "../../src/config/auth-config";
+
+import passport from "passport";
+
+test("configure returns correct result", () => {
+    const components = new WebComponents({ minLogLevel: LogLevel.DEBUG });
+    const authConfig = new AuthConfig(passport, components);
+    const result = authConfig.configure();
+    expect(result).not.toBe(null);
+});
