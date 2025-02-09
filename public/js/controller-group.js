b0c80ead2bb47751d505823290c15d9b07b77950
Piotr Ponikowski <piopon.github@gmail.com>
2025-02-10 00:04:47 +0100

    explicitly setting ALL dimensions of new group column

--- public/js/controller-group.js
+++ public/js/controller-group.js
@@ -372,10 +372,12 @@ export class GroupsController {
       }
     } else {
       if (window.innerWidth > 650) {
+        column.parentNode.style.height = "100%"
         column.parentNode.style.width = this.#groupColumns.length >= 2 ? 45 : "100%";
         column.parentNode.style.left = `${100 - NEW_GROUP_COLUMN_WIDTH}vh`;
       } else {
         column.parentNode.style.height = this.#groupColumns.length >= 2 ? 45 : "100%";
+        column.parentNode.style.width = "100%"
         column.parentNode.style.top = `${100 - NEW_GROUP_COLUMN_WIDTH}vw`;
       }
     }
