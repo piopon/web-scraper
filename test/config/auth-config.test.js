4e5806b5549223ace07b32b93bc242759234f007
Piotr Ponikowski <piopon.github@gmail.com>
2025-03-22 00:15:17 +0100

    reformatted auth-config.test.js file

--- test/config/auth-config.test.js
+++ test/config/auth-config.test.js
@@ -8,13 +8,13 @@ process.env.JWT_SECRET = "test_secret";
 process.env.GOOGLE_CLIENT_ID = "test_id";
 
 test("configure returns correct result", () => {
-    const components = new WebComponents({ minLogLevel: LogLevel.DEBUG });
-    const authConfig = new AuthConfig(passport, components);
-    const result = authConfig.configure();
-    expect(Object.hasOwn(result._strategies, "jwt")).toBe(true);
-    expect(Object.hasOwn(result._strategies, "google")).toBe(true);
-    expect(Object.hasOwn(result._strategies, "local-login")).toBe(true);
-    expect(Object.hasOwn(result._strategies, "local-register")).toBe(true);
+  const components = new WebComponents({ minLogLevel: LogLevel.DEBUG });
+  const authConfig = new AuthConfig(passport, components);
+  const result = authConfig.configure();
+  expect(Object.hasOwn(result._strategies, "jwt")).toBe(true);
+  expect(Object.hasOwn(result._strategies, "google")).toBe(true);
+  expect(Object.hasOwn(result._strategies, "local-login")).toBe(true);
+  expect(Object.hasOwn(result._strategies, "local-register")).toBe(true);
 });
 
 describe("auth object correctly authenticates user", () => {
