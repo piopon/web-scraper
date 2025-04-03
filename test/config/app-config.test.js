093add4b35a8e3c8515caa161c4990109cc6ce54
Piotr Ponikowski <piopon.github@gmail.com>
2025-04-03 20:49:04 +0200

    added new test checking database config for custom values

--- test/config/app-config.test.js
+++ test/config/app-config.test.js
@@ -36,5 +36,21 @@ describe("getConfig", () => {
       expect(testConfig.serverConfig).not.toBe(null);
       expect(testConfig.serverConfig.port).toBe(1234);
     });
+    test("for database config", () => {
+      process.env.DB_ADDRESS = "test-custom-url";
+      process.env.DB_NAME = "test-name";
+      process.env.DB_PORT = 4321;
+      process.env.DB_USER = "test-user";
+      process.env.DB_PASSWORD = "test-password";
+      process.env.DB_TIMEOUT = 66_123;
+      const testConfig = new AppConfig().getConfig();
+      expect(testConfig.databaseConfig).not.toBe(null);
+      expect(testConfig.databaseConfig.url).toBe("test-custom-url");
+      expect(testConfig.databaseConfig.name).toBe("test-name");
+      expect(testConfig.databaseConfig.port).toBe(4321);
+      expect(testConfig.databaseConfig.user).toBe("test-user");
+      expect(testConfig.databaseConfig.password).toBe("test-password");
+      expect(testConfig.databaseConfig.timeout).toBe(66_123);
+    });
   });
 });
