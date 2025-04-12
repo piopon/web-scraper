2f515817f63d3d0d87ac5001a2498978ccd2e9ca
Piotr Ponikowski <piopon.github@gmail.com>
2025-04-13 00:34:28 +0200

    added extra expect statement to verify the redirect destination

--- test/routes/view/auth-router.test.js
+++ test/routes/view/auth-router.test.js
@@ -123,24 +123,28 @@ describe("created auth POST routes", () => {
     const response = await testAgent.post("/auth/register");
     expect(response.statusCode).toBe(302);
     expect(response.text).toBe("Found. Redirecting to /auth/register");
+    expect(response.headers.location).toBe("/auth/register");
   });
   test("returns correct result using /login endpoint", async () => {
     isAuthenticatedResult = false;
     const response = await testAgent.post("/auth/login");
     expect(response.statusCode).toBe(302);
     expect(response.text).toBe("Found. Redirecting to /auth/login");
+    expect(response.headers.location).toBe("/auth/login");
   });
   test("returns correct result using /demo endpoint", async () => {
     isAuthenticatedResult = false;
     const response = await testAgent.post("/auth/demo");
     expect(response.statusCode).toBe(302);
     expect(response.text).toBe("Found. Redirecting to /auth/login");
+    expect(response.headers.location).toBe("/auth/login");
   });
   test("returns correct result using /logout endpoint", async () => {
     isAuthenticatedResult = false;
     const response = await testAgent.post("/auth/logout");
     expect(response.statusCode).toBe(302);
     expect(response.text).toBe("Found. Redirecting to /auth/login");
+    expect(response.headers.location).toBe("/auth/login");
   });
 });
 
