a9ee757c6e9cc298b9e0d10787f59488b19da0d2
Piotr Ponikowski <piopon.github@gmail.com>
2025-04-28 14:29:07 +0200

    refactored getHistory logic to use status object

--- src/components/web-scraper.js
+++ src/components/web-scraper.js
@@ -245,10 +245,10 @@ export class WebScraper {
     }
     // all input correct - perform additional checks and then return history
     const invalidStateMessage = "Invalid internal state";
-    const currentStatus = this.#status.getStatus().message;
+    const currentStatus = this.#status.getStatus();
     if (session.id == null) {
       // scraper is NOT running in selected intervals
-      if (currentStatus.startsWith(WebScraper.#RUNNING_STATUS)) {
+      if (currentStatus.message.startsWith(WebScraper.#RUNNING_STATUS)) {
         // incorrect state - update field
         this.#status.error(invalidStateMessage);
       }
