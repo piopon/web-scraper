d25980957ec6c5fe5dd51bcf145fc9a002de7934
Piotr Ponikowski <piopon.github@gmail.com>
2025-08-28 00:12:15 +0200

    added challenge generator method description

--- src/utils/challenge-utils.js
+++ src/utils/challenge-utils.js
@@ -1,4 +1,9 @@
 export class ChallengeUtils {
+  /**
+   * Method used to generate challenge phrase
+   * @param  {Array} inputs Array of inputs which form challenge
+   * @returns String containing generated challenge phrase
+   */
   static generate(...inputs) {
     return inputs.join(process.env.CHALLENGE_JOIN);
   }
