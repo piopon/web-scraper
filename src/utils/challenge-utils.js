c062b26c0f2ea339b5454105ce1ef6580e14f40a
Piotr Ponikowski <piopon.github@gmail.com>
2025-08-28 00:13:00 +0200

    added logic to shuffle and reverse each input string component

--- src/utils/challenge-utils.js
+++ src/utils/challenge-utils.js
@@ -5,6 +5,14 @@ export class ChallengeUtils {
    * @returns String containing generated challenge phrase
    */
   static generate(...inputs) {
-    return inputs.join(process.env.CHALLENGE_JOIN);
+    return inputs.map((input) => this.#shuffle(input)).join(process.env.CHALLENGE_JOIN);
+  }
+
+  static #shuffle(input) {
+    let chars = input.split("");
+    for (let i = 0; i < chars.length - 1; i += 2) {
+      [chars[i], chars[i + 1]] = [chars[i + 1], chars[i]];
+    }
+    return chars.reverse().join("");
   }
 }
