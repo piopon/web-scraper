2ff20181d8f89e0c3adda188a72cdfcf34ceb00e
Piotr Ponikowski <piopon.github@gmail.com>
2025-09-02 00:21:58 +0200

    new method description in regex utils class

--- src/utils/regex-utils.js
+++ src/utils/regex-utils.js
@@ -1,4 +1,9 @@
 export class RegexUtils {
+  /**
+   * Method used to escape input string characters to be useful for regex
+   * @param {String} input The string which chars we want to escape
+   * @returns string with escaped characters
+   */
   static escape(input) {
     return input.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
   }
