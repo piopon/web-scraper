ea9816189e2ae089cee04b8a5465af14cc4226aa
Piotr Ponikowski <piopon.github@gmail.com>
2025-09-07 12:25:35 +0200

    added default router query and body validation logic

--- src/routes/api/settings-router.js
+++ src/routes/api/settings-router.js
@@ -1,11 +1,47 @@
+import { ScrapConfig } from "../../model/scrap-config.js";
+
+import Ajv from "ajv";
 import express from "express";
 
 export class SettingsRouter {
   createRoutes() {
     const router = express.Router();
     router.post("/import", async (request, response) => {
+      const validationResult = this.#validateQueryParams(request.query);
+      if (!validationResult.valid) {
+        response.status(400).json(validationResult.cause);
+        return;
+      }
+      const bodyValidation = this.#validateBody(request.body);
+      if (!bodyValidation.content) {
+        response.status(400).json(bodyValidation.cause);
+        return;
+      }
       response.status(200).json("OK");
     });
     return router;
+  }
+
+  #validateQueryParams(params) {
+    const queryValidation = new Ajv().compile({
+      type: "object",
+      additionalProperties: false,
+    });
+    return { valid: queryValidation(params), cause: queryValidation.errors };
+  }
+
+  #validateBody(requestBody) {
+    // validate JSON structure of the request body content
+    const schemaValidate = new Ajv().compile(ScrapConfig.getRequestBodySchema());
+    if (!schemaValidate(requestBody)) {
+      return { content: undefined, cause: schemaValidate.errors };
+    }
+    // validate JSON values of the request body content
+    const parsedBody = new ScrapConfig(requestBody);
+    const valueValidate = parsedBody.checkValues().errors;
+    if (valueValidate.length > 0) {
+      return { content: undefined, cause: valueValidate[0] };
+    }
+    return { content: parsedBody, cause: undefined };
   }
 }
