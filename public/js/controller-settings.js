e2ed0cc69c90be4c01b966fb02b6e28181a4d8ef
Piotr Ponikowski <piopon.github@gmail.com>
2025-09-07 11:56:11 +0200

    filled import button change and select handlers

--- public/js/controller-settings.js
+++ public/js/controller-settings.js
@@ -8,13 +8,26 @@ export class SettingsController {
       button.addEventListener("click", console.log("EXPORT APPLIED"));
     });
     document.querySelectorAll("input.config-import-file").forEach((selector) => {
-      selector.addEventListener("change", console.log("FILE CHANGED"));
+      selector.addEventListener("change", this.#changeImportConfigFileHandler);
     });
     document.querySelectorAll("input.config-import-button").forEach((button) => {
-      button.addEventListener("click", console.log("FILE INVOKED"));
+      button.addEventListener("click", this.#selectImportConfigFileHandler);
     });
     document.querySelectorAll("input.config-import-apply").forEach((button) => {
       button.addEventListener("click", console.log("IMPORT APPLIED"));
     });
+  }
+
+  #changeImportConfigFileHandler(event) {
+    if (event.target.files[0]) {
+      const fileButton = event.target.nextElementSibling;
+      fileButton.value = event.target.files[0].name;
+      event.target.nextElementSibling.nextElementSibling.disabled = false;
+    }
+  }
+
+  #selectImportConfigFileHandler(event) {
+    event.target.previousElementSibling.click();
+    event.stopPropagation();
   }
 }
