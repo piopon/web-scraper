3428cc6085881c78660eb4275a6e76842de88832
Piotr Ponikowski <piopon.github@gmail.com>
2025-09-08 22:09:44 +0200

    new logic in settings service to retrieve current user's config for export

--- public/js/service-settings.js
+++ public/js/service-settings.js
@@ -1,6 +1,18 @@
 import { CommonService } from "./service-common.js";
 
 export class SettingsService {
+  static async getConfig() {
+    const url = `/api/v1/config`;
+    const requestOpts = CommonService.createRequestOptions("GET");
+    const response = await fetch(url, requestOpts);
+    if (response.status === 200) {
+      return response.json();
+    }
+    const errorResponse = await response.json();
+    const errorDetails = CommonService.getErrorDetails(errorResponse);
+    throw new Error(`Cannot get current configuration: ${errorDetails}`);
+  }
+
   static async importConfig(configFile) {
     const url = `/api/v1/settings/import`;
     const requestBody = await configFile.files[0].text();
